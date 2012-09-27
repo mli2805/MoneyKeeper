927eb0774c486b04738f956d6c9ac772d20c4b20
Leonid <Leonid@opx-lmarholin>
2012-09-27 11:05:45 +0300

    Init

---
+++ Keeper/AppBootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using Caliburn.Micro;
+
+namespace Keeper
+{
+  public class AppBootstrapper : Bootstrapper<IShell>
+  {
+    private CompositionContainer _container;
+
+    protected override void Configure()
+    {
+      _container = new CompositionContainer(new AggregateCatalog(
+        AssemblySource.Instance.Select(x => new AssemblyCatalog(x))));
+
+      var batch = new CompositionBatch();
+
+      batch.AddExportedValue<IWindowManager>(new WindowManager());
+      batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+      batch.AddExportedValue(_container);
+      
+      _container.Compose(batch);
+    }
+
+    protected override object GetInstance(Type serviceType, string key)
+    {
+      var contract = string.IsNullOrEmpty(key) 
+        ? AttributedModelServices.GetContractName(serviceType) 
+        : key;
+
+      var exports = _container.GetExportedValues<object>(contract);
+
+      var first = exports.FirstOrDefault();
+      if (first != null) return first;
+
+      throw new Exception(string.Format(
+        "Could not locate any instances of contract {0}.", contract));
+    }
+
+    protected override IEnumerable<object> GetAllInstances(Type serviceType)
+    {
+      return _container.GetExportedValues<object>(
+        AttributedModelServices.GetContractName(serviceType));
+    }
+
+    protected override void BuildUp(object instance)
+    {
+      _container.SatisfyImportsOnce(instance);
+    }
+
+    protected override IEnumerable<Assembly> SelectAssemblies()
+    {
+      yield return typeof(AppBootstrapper).Assembly;
+    }
+  }
+}
---
+++ Keeper/ViewModels/ShellViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace Keeper.ViewModels
+{
+  [Export(typeof(IShell))]
+  public class ShellViewModel : Screen, IShell
+  {
+    [Import]
+    public IWindowManager MyWindowManager { get; set; }
+
+    public ShellViewModel()
+    {
+      _message = "Keeper is running (On Debug)";
+    }
+
+    protected override void OnViewLoaded(object view)
+    {
+      DisplayName = "Keeper 2012";
+    }
+
+    private string _message;
+    public String Message
+    {
+      get { return _message; }
+      set
+      {
+        if (value == _message) return;
+        _message = value;
+        NotifyOfPropertyChange(() => Message);
+      }
+    }
+
+    public void RunOperationsForm()
+    {
+      String arcMessage = Message;
+      Message = "Input operations";
+      MyWindowManager.ShowDialog(new AddOperationViewModel());
+      Message = arcMessage;
+    }
+
+    public void CloseProgram()
+    {
+      if (MessageBox.Show("Are you sure?","",MessageBoxButton.YesNo,MessageBoxImage.Question) == MessageBoxResult.Yes )
+        TryClose();
+    }
+
+  }
+}
