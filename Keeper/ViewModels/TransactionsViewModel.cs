fe0f958229c0f607157ec9d6cc9c4fc98229af63
Leonid <Leonid@opx-lmarholin>
2012-11-01 17:16:48 +0300

    ?? ????? ?????? ? 31

--- Keeper/ViewModels/TransactionsViewModel.cs
+++ Keeper/ViewModels/TransactionsViewModel.cs
@@ -11,23 +11,11 @@ namespace Keeper.ViewModels
     public KeeperDb Db { get { return IoC.Get<KeeperDb>(); } }
 
     public ObservableCollection<Transaction> Rows { get; set; }
-
-    public List<Transaction> TestList { get; set; }
-
+    
     public TransactionsViewModel()
     {
-
       Db.Transactions.Load();
       Rows = Db.Transactions.Local;
-
-      TestList = new List<Transaction>();
-      for (int i=0; i<10; i++)
-      {
-        var tr = new Transaction();
-        tr.Comment = "test" + i;
-        TestList.Add(tr);
-      }
-
     }
   }
 }
