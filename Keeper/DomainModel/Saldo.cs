481554a6640e9c4c29e4668b24a087661d61d240
Leonid <Leonid@opx-lmarholin>
2013-02-20 10:45:26 +0300

    ????????? ????? ???????

--- Keeper/DomainModel/Saldo.cs
+++ Keeper/DomainModel/Saldo.cs
@@ -9,11 +9,16 @@ namespace Keeper.DomainModel
     public decimal Incomes { get; set; }
     public decimal Expense { get; set; }
     public decimal LargeExpense { get; set; }
-    public decimal ExchangeDifference { get { return EndBalance - BeginBalance - Incomes - Expense; } }
+    public decimal ExchangeDifference { get { return EndBalance - BeginBalance - Incomes + Expense; } }
     public decimal EndBalance { get; set; }
     public decimal LastByrRate { get; set; }
     public DateTime LastDayWithTransactionsInMonth { get; set; }
-    public decimal SaldoIncomesExpense { get { return Incomes + Expense; } }
+    public decimal SaldoIncomesExpense { get { return Incomes - Expense; } }
     public decimal Result { get { return EndBalance - BeginBalance; } }
+
+    public decimal ForecastIncomes { get; set; }
+    public decimal ForecastExpense { get; set; }
+    public decimal ForecastFinResult { get { return ForecastIncomes - ForecastExpense; } }
+    public decimal ForecastEndBalance { get { return BeginBalance + ForecastFinResult; } }
   }
 }
