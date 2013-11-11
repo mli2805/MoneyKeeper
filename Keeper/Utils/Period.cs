724762b6ce284fc5733c678eb99d4071d6cee27b
mli2805 <mli2805@gmail.com>
2013-11-11 21:26:03 +0300

    Period constructor expand dates to the "0:00" time

--- Keeper/Utils/Period.cs
+++ Keeper/Utils/Period.cs
@@ -9,10 +9,18 @@ namespace Keeper.Utils
     private readonly DateTime _start;
     private readonly DateTime _finish;
 
+    private DateTime GetDayBegin(DateTime day)
+    {
+      var yy = day.Year;
+      var mm = day.Month;
+      var dd = day.Day;
+      return new DateTime(yy, mm, dd, 0, 0, 0, 0);
+    }
+
     public Period(DateTime start, DateTime finish)
     {
-      _start = start;
-      _finish = finish;
+      _start = GetDayBegin(start);
+      _finish = GetDayBegin(finish.AddDays(1)).AddMilliseconds(-1);
     }
 
     public bool IsDateTimeIn(DateTime checkDate)
