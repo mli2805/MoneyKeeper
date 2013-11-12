6ea11b839bf84576ab37c05844edd8dbf125d55e
Leonid <Leonid@opx-lmarholin>
2013-11-12 16:00:52 +0300

    Date / period selection controls are ready

--- Keeper/Controls/ShellViewDateSelectControl.xaml.cs
+++ Keeper/Controls/ShellViewDateSelectControl.xaml.cs
@@ -7,18 +7,8 @@ namespace Keeper.Controls
   /// <summary>
   /// Interaction logic for ShellViewDateSelectControl.xaml
   /// </summary>
-  public partial class ShellViewDateSelectControl : UserControl
+  public partial class ShellViewDateSelectControl
   {
-    public static readonly DependencyProperty ControlVisibilityProperty =
-           DependencyProperty.Register("ControlVisibility", typeof(Visibility),
-           typeof(ShellViewDateSelectControl), new FrameworkPropertyMetadata(new Visibility()));
-
-    public Visibility ControlVisibility
-    {
-      get { return (Visibility)GetValue(ControlVisibilityProperty); }
-      set { SetValue(ControlVisibilityProperty, value); }
-    }
-
     public static readonly DependencyProperty SelectedDayProperty =
            DependencyProperty.Register("SelectedDay", typeof(DateTime),
            typeof(ShellViewDateSelectControl), new FrameworkPropertyMetadata(new DateTime()));
@@ -26,12 +16,23 @@ namespace Keeper.Controls
     public DateTime SelectedDay
     {
       get { return (DateTime)GetValue(SelectedDayProperty); }
-      set { SetValue(SelectedDayProperty, value); }
+      set
+      {
+        SetValue(SelectedDayProperty, value);
+        MyDatePicker.SelectedDate = value;
+      }
     }
 
     public ShellViewDateSelectControl()
     {
       InitializeComponent();
+
+      Loaded += ShellViewDateSelectControlLoaded;
+    }
+
+    void ShellViewDateSelectControlLoaded(object sender, RoutedEventArgs e)
+    {
+      SelectedDay = DateTime.Today;
     }
 
     public void Connect(int connectionId, object target)
@@ -39,52 +40,21 @@ namespace Keeper.Controls
       throw new NotImplementedException();
     }
 
-    private void OneDayBeforeClick(object sender, RoutedEventArgs e)
+    private void OneDayBeforeClick(object sender, RoutedEventArgs e) { SelectedDay = SelectedDay.AddDays(-1); }
+    private void OneMonthBeforeClick(object sender, RoutedEventArgs e) { SelectedDay = SelectedDay.AddMonths(-1); }
+    private void OneYearBeforeClick(object sender, RoutedEventArgs e) { SelectedDay = SelectedDay.AddYears(-1); }
+    private void OneDayAfterClick(object sender, RoutedEventArgs e) { SelectedDay = SelectedDay.AddDays(1); }
+    private void OneMonthAfterClick(object sender, RoutedEventArgs e) { SelectedDay = SelectedDay.AddMonths(1); }
+    private void OneYearAfterClick(object sender, RoutedEventArgs e) { SelectedDay = SelectedDay.AddYears(1); }
+    private void TodayClick(object sender, RoutedEventArgs e) { SelectedDay = DateTime.Today; }
+    private void YesterdayClick(object sender, RoutedEventArgs e) { SelectedDay = DateTime.Today.AddDays(-1); }
+    private void LastMonthEndClick(object sender, RoutedEventArgs e) { SelectedDay = DateTime.Today.AddDays(-DateTime.Today.Day + 1); }
+    private void MyDatePickerSelectedDateChanged(object sender, SelectionChangedEventArgs e) 
     {
-      SelectedDay = SelectedDay.AddDays(-1);
+      if (MyDatePicker.SelectedDate == null) MyDatePicker.SelectedDate = SelectedDay;
+      else
+        SelectedDay = (DateTime)MyDatePicker.SelectedDate;
     }
-
-    private void OneMonthBeforeClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = SelectedDay.AddMonths(-1);
-    }
-
-    private void OneYearBeforeClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = SelectedDay.AddYears(-1);
-    }
-
-    private void OneDayAfterClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = SelectedDay.AddDays(1); 
-    }
-
-    private void OneMonthAfterClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = SelectedDay.AddMonths(1);
-    }
-
-    private void OneYearAfterClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = SelectedDay.AddYears(1);
-    }
-
-    private void TodayClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = DateTime.Today;
-    }
-
-    private void YesterdayClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = DateTime.Today.AddDays(-1);
-    }
-
-    private void LastMonthEndClick(object sender, RoutedEventArgs e)
-    {
-      SelectedDay = DateTime.Today.AddDays(-DateTime.Today.Day + 1);
-    }
-
-
 
   }
 }
--- Keeper/Controls/ShellViewPeriodSelectControl.xaml.cs
+++ Keeper/Controls/ShellViewPeriodSelectControl.xaml.cs
@@ -8,11 +8,33 @@ namespace Keeper.Controls
   /// <summary>
   /// Interaction logic for ShellViewPeriodSelectControl.xaml
   /// </summary>
-  public partial class ShellViewPeriodSelectControl : UserControl
+  public partial class ShellViewPeriodSelectControl
   {
+    public static readonly DependencyProperty SelectedPeriodProperty =
+           DependencyProperty.Register("SelectedPeriod", typeof(Period),
+           typeof(ShellViewPeriodSelectControl), new FrameworkPropertyMetadata(new Period()));
+
+    public Period SelectedPeriod
+    {
+      get { return (Period)GetValue(SelectedPeriodProperty); }
+      set
+      {
+        SetValue(SelectedPeriodProperty, value);
+        StartDatePicker.SelectedDate = value.GetStart();
+        FinishDatePicker.SelectedDate = value.GetFinish();
+      }
+    }
+
     public ShellViewPeriodSelectControl()
     {
       InitializeComponent();
+
+      Loaded += ShellViewPeriodSelectControlLoaded;
+    }
+
+    void ShellViewPeriodSelectControlLoaded(object sender, RoutedEventArgs e)
+    {
+      SelectedPeriod = new Period(DateTime.Today.AddDays(-(DateTime.Today.Day-1)), DateTime.Today.AddDays(1).AddSeconds(-1));
     }
 
     public void Connect(int connectionId, object target)
@@ -20,26 +42,61 @@ namespace Keeper.Controls
       throw new NotImplementedException();
     }
 
-    public static readonly DependencyProperty ControlVisibilityProperty =
-       DependencyProperty.Register("ControlVisibility", typeof(Visibility),
-       typeof(ShellViewDateSelectControl), new FrameworkPropertyMetadata(new Visibility()));
+    private void OneDayBeforeClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(SelectedPeriod.GetStart().AddDays(-1), SelectedPeriod.GetFinish().AddDays(-1)); }
 
-    public Visibility ControlVisibility
+    private void OneMonthBeforeClick(object sender, RoutedEventArgs e)
     {
-      get { return (Visibility)GetValue(ControlVisibilityProperty); }
-      set { SetValue(ControlVisibilityProperty, value); }
+
+      var finish = SelectedPeriod.GetFinish();
+      finish = IsLastDayOfMonth(finish) ? finish.AddDays(-finish.Day) : finish.AddMonths(-1);
+      SelectedPeriod = new Period(SelectedPeriod.GetStart().AddMonths(-1), finish);
     }
 
-    public static readonly DependencyProperty SelectedPeriodProperty =
-           DependencyProperty.Register("SelectedPeriod", typeof(Period),
-           typeof(ShellViewDateSelectControl), new FrameworkPropertyMetadata(new Period()));
-
-    public Period SelectedPeriod
+    private void OneYearBeforeClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(SelectedPeriod.GetStart().AddYears(-1), SelectedPeriod.GetFinish().AddYears(-1)); }
+    private void OneDayAfterClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(SelectedPeriod.GetStart().AddDays(1), SelectedPeriod.GetFinish().AddDays(1)); }
+    private void OneMonthAfterClick(object sender, RoutedEventArgs e)
     {
-      get { return (Period)GetValue(SelectedPeriodProperty); }
-      set { SetValue(SelectedPeriodProperty, value); }
+      DateTime finish;
+      if (IsLastDayOfMonth(SelectedPeriod.GetFinish()))
+      {
+        finish = SelectedPeriod.GetFinish().AddMonths(2);
+        finish = finish.AddDays(-finish.Day);
+      }
+      else finish = SelectedPeriod.GetFinish().AddMonths(1);
+
+      SelectedPeriod = new Period(SelectedPeriod.GetStart().AddMonths(1), finish);
+    }
+    private void OneYearAfterClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(SelectedPeriod.GetStart().AddYears(1), SelectedPeriod.GetFinish().AddYears(1)); }
+    private void TodayClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(DateTime.Today, DateTime.Today); }
+    private void YesterdayClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(DateTime.Today.AddDays(-1), DateTime.Today.AddDays(-1)); }
+    private void ThisMonthClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(DateTime.Today.AddDays(-DateTime.Today.Day + 1), DateTime.Today); }
+    private void LastMonthClick(object sender, RoutedEventArgs e)
+    {
+      var finish = DateTime.Today.AddDays(-DateTime.Today.Day);
+      SelectedPeriod = new Period(finish.AddDays(-finish.Day + 1), finish);
+    }
+    private void ThisYearPaymentsClick(object sender, RoutedEventArgs e) { SelectedPeriod = new Period(DateTime.Today.AddDays(-DateTime.Today.DayOfYear + 1), DateTime.Today); }
+    private void LastYearPaymentsClick(object sender, RoutedEventArgs e)
+    {
+      var finish = DateTime.Today.AddDays(-DateTime.Today.DayOfYear);
+      SelectedPeriod = new Period(finish.AddDays(-finish.DayOfYear + 1), finish);
     }
 
+    private bool IsLastDayOfMonth(DateTime date) { return date.Month != date.AddDays(1).Month; }
+
+    private void StartDatePickerSelectedDateChanged(object sender, SelectionChangedEventArgs e)
+    {
+      if (StartDatePicker.SelectedDate == null) StartDatePicker.SelectedDate = SelectedPeriod.GetStart();
+      else
+        SelectedPeriod = new Period((DateTime)StartDatePicker.SelectedDate, SelectedPeriod.GetFinish());
+    }
+
+    private void FinishDatePickerSelectedDateChanged(object sender, SelectionChangedEventArgs e)
+    {
+      if (FinishDatePicker.SelectedDate == null) FinishDatePicker.SelectedDate = SelectedPeriod.GetFinish(); 
+      else
+        SelectedPeriod = new Period(SelectedPeriod.GetStart(), (DateTime)FinishDatePicker.SelectedDate);
+    }
 
   }
 }
