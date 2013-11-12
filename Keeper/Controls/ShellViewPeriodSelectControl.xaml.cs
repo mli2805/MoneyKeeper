350f57c52d364517f6e6648ec3f07135234d9d9d
mli2805 <mli2805@gmail.com>
2013-11-12 08:47:54 +0300

    new controls

---
+++ Keeper/Controls/ShellViewPeriodSelectControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Keeper.Utils;
+
+namespace Keeper.Controls
+{
+  /// <summary>
+  /// Interaction logic for ShellViewPeriodSelectControl.xaml
+  /// </summary>
+  public partial class ShellViewPeriodSelectControl : UserControl
+  {
+    public ShellViewPeriodSelectControl()
+    {
+      InitializeComponent();
+    }
+
+    public void Connect(int connectionId, object target)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static readonly DependencyProperty ControlVisibilityProperty =
+       DependencyProperty.Register("ControlVisibility", typeof(Visibility),
+       typeof(ShellViewDateSelectControl), new FrameworkPropertyMetadata(new Visibility()));
+
+    public Visibility ControlVisibility
+    {
+      get { return (Visibility)GetValue(ControlVisibilityProperty); }
+      set { SetValue(ControlVisibilityProperty, value); }
+    }
+
+    public static readonly DependencyProperty SelectedPeriodProperty =
+           DependencyProperty.Register("SelectedPeriod", typeof(Period),
+           typeof(ShellViewDateSelectControl), new FrameworkPropertyMetadata(new Period()));
+
+    public Period SelectedPeriod
+    {
+      get { return (Period)GetValue(SelectedPeriodProperty); }
+      set { SetValue(SelectedPeriodProperty, value); }
+    }
+
+
+  }
+}
