cfd81592a0b1976f4a9ac752459dfaac7a50e86f
mli2805 <mli2805@gmail.com>
2013-12-15 22:06:57 +0300

    more correct branch

---
+++ Keeper/Utils/Dialogs/MyOpenFileDialog.cs
@@ -0,0 +1,16 @@
+﻿using System.Composition;
+
+namespace Keeper.Utils.Dialogs
+{
+	[Export(typeof(IMyOpenFileDialog))]
+	class MyOpenFileDialog : IMyOpenFileDialog
+	{
+		public string Show(string defaultExt, string filter, string filename, string defaultPath)
+		{
+			var dialog = new Microsoft.Win32.OpenFileDialog { DefaultExt = defaultExt, Filter = filter };
+			var result = dialog.ShowDialog();
+			filename = result == true ? dialog.FileName : defaultPath;
+			return filename;
+		}		
+	}
+}
