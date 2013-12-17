d785383ac3ee9006259f7a4e6a027d4ba0681d2a
mli2805 <mli2805@gmail.com>
2013-12-17 19:04:41 +0300

    first test passed!

--- Keeper.UnitTests/Utils/TestBalanceCalculator.cs
+++ Keeper.UnitTests/Utils/TestBalanceCalculator.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Composition;
 using FakeItEasy;
 using Keeper.DomainModel;
 using Keeper.Utils;
+using Keeper.Utils.DbInputOutput;
 using NUnit.Framework;
 using FluentAssertions;
 
@@ -14,16 +16,24 @@ namespace Keeper.UnitTests.Utils
     private IKeeperDb _db;
     private BalanceCalculator _underTest;
 
+    public TestBalanceCalculator()
+    {
+      _db = new DbSerializer().DecryptAndDeserialize("BaseForUnitTests.dbx");
+    }
+
     [Test]
     public void ArticleBalancePairs_Should_Evaluate_Traffic_Within_Period()
     {
-      _db = A.Fake<IKeeperDb>();
       _underTest = new BalanceCalculator(_db);
 
-      var wantedResult = new List<MoneyPair> {new MoneyPair{Amount = 100, Currency = CurrencyCodes.EUR}};
+      var wantedResult = new List<MoneyPair> 
+      {
+        new MoneyPair{Amount = 700, Currency = CurrencyCodes.USD},
+        new MoneyPair{Amount = 6855187, Currency = CurrencyCodes.BYR}
+      };
 
-      var testedCategory  = new Account("Test category");
-      _underTest.ArticleBalancePairs(testedCategory, new Period(new DateTime(0), DateTime.Today)).ShouldBeEquivalentTo(wantedResult);
+      var testedCategory = new Account("Зарплата");
+      _underTest.ArticleBalancePairs(testedCategory, new Period(new DateTime(2013, 11, 1), new DateTime(2013,12,1))).ShouldBeEquivalentTo(wantedResult);
     }
   }
 }
