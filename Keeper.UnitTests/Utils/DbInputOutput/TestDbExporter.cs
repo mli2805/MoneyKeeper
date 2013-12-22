538c98d06195241122de1b2ac16ec76249fe0774
marhoily <marhoily@localhost>
2013-12-22 16:33:51 +0300

    wasn't saved

--- Keeper.UnitTests/Utils/DbInputOutput/TestDbExporter.cs
+++ Keeper.UnitTests/Utils/DbInputOutput/TestDbExporter.cs
@@ -1,8 +1,11 @@
 ﻿using FakeItEasy;
 
+using Keeper.DomainModel;
 using Keeper.Utils.DbInputOutput;
 
 using NUnit.Framework;
+
+using FluentAssertions;
 
 namespace Keeper.UnitTests.Utils.DbInputOutput
 {
@@ -11,18 +14,30 @@ namespace Keeper.UnitTests.Utils.DbInputOutput
 	{
 		DbExporter mUnderTest;
 		IDbGeneralLoader mDbGeneralLoader;
+		DbLoadResult mSpecificResult;
+		KeeperDb mSpecificKeeperDb;
 
 		[SetUp]
 		public void SetUp()
 		{
+			mSpecificKeeperDb = new KeeperDb();
+			mSpecificResult = new DbLoadResult(mSpecificKeeperDb);
 			mDbGeneralLoader = A.Fake<IDbGeneralLoader>();
+			A.CallTo(() => mDbGeneralLoader.LoadByExtension()).Returns(mSpecificResult);
 			mUnderTest = new DbExporter(mDbGeneralLoader);
 		}
 
 		[Test]
-		public void Ctor_Should_Assing_LoadResult()
+		public void Ctor_Should_Assing_LoadResult_What_LoadByExtension_Returns()
 		{
-			Assert.That(mUnderTest, Is.Not.Null);
+			mUnderTest.LoadResult.Should().Be(mSpecificResult);
+		}
+
+		[Test]
+		public void Ctor_Should_Assing_Db_With_Db_From_LoadResult()
+		{
+
+			mUnderTest.Db.Should().Be(mSpecificKeeperDb);
 		}
 	}
 }
