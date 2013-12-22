a3da7678d2bcf90116af48ad9552b82658c3acfd
marhoily <marhoily@localhost>
2013-12-22 16:33:12 +0300

    TestDbExporter

---
+++ Keeper.UnitTests/Utils/DbInputOutput/TestDbExporter.cs
@@ -0,0 +1,28 @@
+﻿using FakeItEasy;
+
+using Keeper.Utils.DbInputOutput;
+
+using NUnit.Framework;
+
+namespace Keeper.UnitTests.Utils.DbInputOutput
+{
+	[TestFixture]
+	public sealed class TestDbExporter
+	{
+		DbExporter mUnderTest;
+		IDbGeneralLoader mDbGeneralLoader;
+
+		[SetUp]
+		public void SetUp()
+		{
+			mDbGeneralLoader = A.Fake<IDbGeneralLoader>();
+			mUnderTest = new DbExporter(mDbGeneralLoader);
+		}
+
+		[Test]
+		public void Ctor_Should_Assing_LoadResult()
+		{
+			Assert.That(mUnderTest, Is.Not.Null);
+		}
+	}
+}
