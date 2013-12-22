455c8cd6fdd73734fca5c51445ef500729f5c6c5
marhoily <marhoily@localhost>
2013-12-22 12:41:07 +0300

    Move classes into matching files

--- Keeper/Utils/DbInputOutput/DbFromZipLoader.cs
+++ Keeper/Utils/DbInputOutput/DbFromZipLoader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Composition;
 using System.IO;
 using Ionic.Zip;
@@ -14,12 +15,14 @@ namespace Keeper.Utils.DbInputOutput
 	{
 		readonly IDbFromTxtLoader mDbFromTxtLoader;
 		readonly IFileSystem mFileSystem;
+		readonly IFileExistenceChecker mExistenceChecker;
 
 		[ImportingConstructor]
-		public DbFromZipLoader(IDbFromTxtLoader dbFromTxtLoader, IFileSystem fileSystem)
+		public DbFromZipLoader(IDbFromTxtLoader dbFromTxtLoader, IFileSystem fileSystem, IFileExistenceChecker existenceChecker)
 		{
 			mDbFromTxtLoader = dbFromTxtLoader;
 			mFileSystem = fileSystem;
+			mExistenceChecker = existenceChecker;
 		}
 
 		public DbLoadError LoadDbFromZip(ref KeeperDb db, string zipFile)
@@ -34,24 +37,21 @@ namespace Keeper.Utils.DbInputOutput
 			var file = mFileSystem.GetFile(zipFilename);
 			using (var zipFile = file.ReadZip())
 			{
-				// here, we extract every entry, but we could extract conditionally
-				// based on entry name, size, date, checkbox status, etc.  
 				foreach (var innerFile in zipFile)
-				{
 					if (!innerFile.ExtractWithPassword(unpackDirectory, ExtractExistingFileAction.OverwriteSilently, "!opa1526"))
 						return new DbLoadError { Code = 21, Explanation = "Bad password!" };
-				}
 			}
-			if (!mFileSystem.GetFile(mFileSystem.PathCombine(Settings.Default.TemporaryTxtDbPath, "Accounts.txt")).Exists)
-				return new DbLoadError { Code = 215, Explanation = "Accounts.txt not found!" };
-			if (!mFileSystem.GetFile(mFileSystem.PathCombine(Settings.Default.TemporaryTxtDbPath, "ArticlesAssociations.txt")).Exists)
-				return new DbLoadError { Code = 225, Explanation = "ArticlesAssociations.txt not found!" };
-			if (!mFileSystem.GetFile(mFileSystem.PathCombine(Settings.Default.TemporaryTxtDbPath, "CurrencyRates.txt")).Exists)
-				return new DbLoadError { Code = 235, Explanation = "CurrencyRates.txt not found!" };
-			if (!mFileSystem.GetFile(mFileSystem.PathCombine(Settings.Default.TemporaryTxtDbPath, "Transactions.txt")).Exists)
-				return new DbLoadError { Code = 245, Explanation = "Transactions.txt not found!" };
-			return new DbLoadError();
+			return mExistenceChecker.Check(mFiles);
 		}
+	
+
+		private readonly Dictionary<string, int> mFiles = new Dictionary<string, int>
+			{
+				{ "Accounts.txt", 215 },
+				{ "ArticlesAssociations.txt", 225 },
+				{ "CurrencyRates.txt", 235 },
+				{ "Transactions.txt", 245 },
+			};
 
 	}
 }
