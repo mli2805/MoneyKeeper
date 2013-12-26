ea65408f1721e840cb75e89dda9de6cea070a366
Leonid <Leonid@opx-lmarholin>
2013-12-26 12:51:47 +0300

    long operation freezes gui

--- LongOperationExample/ShellViewModel.cs
+++ LongOperationExample/ShellViewModel.cs
@@ -1,3 +1,50 @@
-namespace LongOperationExample {
-    public class ShellViewModel : IShell {}
+using System.IO;
+using Caliburn.Micro;
+
+namespace LongOperationExample 
+{
+    public class ShellViewModel : Screen, IShell
+    {
+      private string _statusBarItem0;
+      private string _statusBarItem1;
+      public string StatusBarItem0
+      {
+        get { return _statusBarItem0; }
+        set
+        {
+          if (value == _statusBarItem0) return;
+          _statusBarItem0 = value;
+          NotifyOfPropertyChange(() => StatusBarItem0);
+        }
+      }
+      public string StatusBarItem1
+      {
+        get { return _statusBarItem1; }
+        set
+        {
+          if (value == _statusBarItem1) return;
+          _statusBarItem1 = value;
+          NotifyOfPropertyChange(() => StatusBarItem1);
+        }
+      }
+
+
+      public ShellViewModel()
+      {
+        StatusBarItem0 = "Idle";
+      }
+
+      public void StartLongOperation()
+      {
+        StatusBarItem0 = "Long operation is started...";
+        LongOperationProcess();
+        StatusBarItem0 = "Long operation has ended.";
+      }
+
+      public void LongOperationProcess()
+      {
+        var store = File.ReadAllBytes(@"h:\t2300.sql");
+        File.WriteAllBytes(@"h:\q_t2300.sql",store);
+      }
+    }
 }
