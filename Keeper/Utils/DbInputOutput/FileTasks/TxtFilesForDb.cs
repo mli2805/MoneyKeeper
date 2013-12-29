81b388a2c03a19c4ceb88b3625f313b0de6572b2
marhoily <marhoily@localhost>
2013-12-29 11:13:49 +0300

    fixed TestDbUnzipper

--- Keeper/Utils/DbInputOutput/FileTasks/TxtFilesForDb.cs
+++ Keeper/Utils/DbInputOutput/FileTasks/TxtFilesForDb.cs
@@ -2,20 +2,19 @@ using System.Collections.Generic;
 
 namespace Keeper.Utils.DbInputOutput.ZipTasks
 {
-  public static class TxtFilesForDb
-  {
-    public static Dictionary<string, int> Dict
-    {
-      get
-      {
-        return new Dictionary<string, int>
-                 {
-                   { "Accounts.txt", 215 },
-                   { "ArticlesAssociations.txt", 225 },
-                   { "CurrencyRates.txt", 235 },
-                   { "Transactions.txt", 245 },
-                 };
-      }
-    }
-  }
+	public static class TxtFilesForDb
+	{
+		static readonly Dictionary<string, int> sDictionary = new Dictionary<string, int>
+		  {
+			  { "Accounts.txt", 215 },
+			  { "ArticlesAssociations.txt", 225 },
+			  { "CurrencyRates.txt", 235 },
+			  { "Transactions.txt", 245 },
+		  };
+
+		public static Dictionary<string, int> Dict
+		{
+			get { return sDictionary; }
+		}
+	}
 }
