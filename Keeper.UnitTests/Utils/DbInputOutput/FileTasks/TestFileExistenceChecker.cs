544082bad5cb6d2662e6c857beb1027cf7916e6e
marhoily <marhoily@localhost>
2013-12-29 12:12:46 +0300

    fixed TestFileExistenceChecker

--- Keeper.UnitTests/Utils/DbInputOutput/FileTasks/TestFileExistenceChecker.cs
+++ Keeper.UnitTests/Utils/DbInputOutput/FileTasks/TestFileExistenceChecker.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 using FakeItEasy;
 
 using FluentAssertions;
 
 using Keeper.Properties;
+using Keeper.Utils.DbInputOutput.CompositeTasks;
 using Keeper.Utils.DbInputOutput.FileTasks;
 using Keeper.Utils.DbInputOutput.ZipTasks;
 using Keeper.Utils.FileSystem;
@@ -11,44 +14,48 @@ using NUnit.Framework;
 
 namespace Keeper.UnitTests.Utils.DbInputOutput.FileTasks
 {
-  [TestFixture]
-  public sealed class TestFileExistenceChecker
-  {
-    FileExistenceChecker _underTest;
-    IFileSystem _fileSystem;
-    IFile _file;
+	[TestFixture]
+	public sealed class TestFileExistenceChecker
+	{
+		FileExistenceChecker _underTest;
+		IFileSystem _fileSystem;
+		IFile _file;
 
+		static readonly Dictionary<string, int> sFiles = new Dictionary<string, int>
+		  {
+			  { "Accounts.txt", 215 },
+		  };
 
-    [SetUp]
-    public void SetUp()
-    {
-      _fileSystem = A.Fake<IFileSystem>();
-      _file = A.Fake<IFile>();
-      _underTest = new FileExistenceChecker(_fileSystem);
-    }
+		[SetUp]
+		public void SetUp()
+		{
+			_fileSystem = A.Fake<IFileSystem>();
+			_file = A.Fake<IFile>();
+			_underTest = new FileExistenceChecker(_fileSystem);
 
-    [Test]
-    public void Check_Should_Return_Null_If_All_Right()
-    {
-      // Arrange
-      A.CallTo(() => _fileSystem.GetFile(A<string>.Ignored)).Returns(_file);
-      A.CallTo(() => _file.Exists).Returns(true);
+			Settings.Default.TemporaryTxtDbPath = "temp path";
+			A.CallTo(() => _fileSystem.PathCombine("temp path", "Accounts.txt")).Returns("combined path");
+			A.CallTo(() => _fileSystem.GetFile("combined path")).Returns(_file);
+		}
 
-      //Action & Assert
-      _underTest.Check(TxtFilesForDb.Dict).Should().BeNull();
-    }
+		[Test]
+		public void Check_If_All_Right_Should_Return_Null()
+		{
+			// Arrange
+			A.CallTo(() => _file.Exists).Returns(true);
 
-    [Test]
-    public void Check_When_File_Is_Absent_Should_Return_Negative_DbLoadResult_With_His_Name_And_Appropriate_In_Explanation()
-    {
-      // Arrange
-      A.CallTo(() => _fileSystem.GetFile(_fileSystem.PathCombine(Settings.Default.TemporaryTxtDbPath, "Account.txt"))).Returns(_file);
-      A.CallTo(() => _file.Exists).Returns(false);
+			//Action & Assert
+			_underTest.Check(sFiles).Should().BeNull();
+		}
 
-      //Action & Assert
-      var testResult = _underTest.Check(TxtFilesForDb.Dict);
-      testResult.Explanation.Should().Be("Accounts.txt not found!");
-      testResult.Code.Should().Be(215);
-    }
-  }
+		[Test]
+		public void Check_When_File_Is_Absent_Should_Return_Negative_DbLoadResult_With_His_Name_And_Appropriate_Explanation()
+		{
+			// Arrange
+			A.CallTo(() => _file.Exists).Returns(false);
+
+			//Action & Assert
+			_underTest.Check(sFiles).ShouldBeEquivalentTo(new DbLoadResult(215, "Accounts.txt not found!"));
+		}
+	}
 }
