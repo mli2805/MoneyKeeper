24b8d4aa579dc4c16f8bc3f24a6c535e115d9da8
marhoily <marhoily@localhost>
2014-01-07 13:05:19 +0300

    Refactor Gardener into IdGenerator, Integrity, Operations

--- Keeper/Utils/Accounts/MyFactory.cs
+++ Keeper/Utils/Accounts/MyFactory.cs
@@ -4,23 +4,30 @@ using Keeper.ViewModels;
 
 namespace Keeper.Utils.Accounts
 {
-  [Export (typeof(IMyFactory))]
-  public class MyFactory : IMyFactory
-  {
-    public AddAndEditAccountViewModel CreateAddAndEditAccountViewModel(Account account, string title)
-    {
-      return new AddAndEditAccountViewModel(account, title);
-    }
+	[Export(typeof(IMyFactory))]
+	public class MyFactory : IMyFactory
+	{
+		public AddAndEditAccountViewModel CreateAddAndEditAccountViewModel(Account account, string title)
+		{
+			return new AddAndEditAccountViewModel(account, title);
+		}
 
-    public Account CreateAccount()
-    {
-      return new Account();
-    }
+		public Account CreateAccount()
+		{
+			return new Account();
+		}
 
-    public Account CreateAccount(Account parent)
-    {
-      return new Account(){Parent = parent};
-    }
+		public Account CloneAccount(Account account)
+		{
+			var result = CreateAccount();
+			Account.CopyForEdit(result, account);
+			return result;
+		}
 
-  }
+		public Account CreateAccount(Account parent)
+		{
+			return new Account() { Parent = parent };
+		}
+
+	}
 }
