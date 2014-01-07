ddd598e473fe65d16616e1e52831f52405f9fcd1
marhoily <marhoily@localhost>
2014-01-07 14:01:42 +0300

    main menu as control works

--- Keeper/Utils/DbInputOutput/TxtTasks/DbTxtFileWriter.cs
+++ Keeper/Utils/DbInputOutput/TxtTasks/DbTxtFileWriter.cs
@@ -7,28 +7,28 @@ using Keeper.Utils.FileSystem;
 
 namespace Keeper.Utils.DbInputOutput.TxtTasks
 {
-  [Export (typeof(IDbTxtFileWriter))]
+	[Export(typeof(IDbTxtFileWriter))]
 	public class DbTxtFileWriter : IDbTxtFileWriter
-  {
+	{
 		readonly IFileSystem mFileSystem;
-    private readonly IMySettings _mySettings;
-    readonly Encoding mEncoding1251 = Encoding.GetEncoding(1251);
+		private readonly IMySettings _mySettings;
+		readonly Encoding mEncoding1251 = Encoding.GetEncoding(1251);
 
 		[ImportingConstructor]
 		public DbTxtFileWriter(IFileSystem fileSystem, IMySettings mySettings)
 		{
-		  mFileSystem = fileSystem;
-		  _mySettings = mySettings;
+			mFileSystem = fileSystem;
+			_mySettings = mySettings;
 		}
 
-    public void WriteDbFile(string filename, IEnumerable<string> content)
-    {
-      var path = (string)_mySettings.GetSetting("TemporaryTxtDbPath");
+		public void WriteDbFile(string filename, IEnumerable<string> content)
+		{
+			var path = (string)_mySettings.GetSetting("TemporaryTxtDbPath");
 			var directory = mFileSystem.GetDirectory(path);
 			if (!directory.Exists) directory.Create();
 
 			var fullPath = mFileSystem.PathCombine(path, filename);
-      mFileSystem.GetFile(fullPath).WriteAllLines(content, mEncoding1251);
+			mFileSystem.GetFile(fullPath).WriteAllLines(content, mEncoding1251);
 		}
 	}
 }
