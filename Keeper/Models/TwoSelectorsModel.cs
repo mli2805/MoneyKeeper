69d025255611ec63184af6f9430a170afb64b546
Leonid <Leonid@opx-lmarholin>
2014-01-22 15:15:46 +0300

    fixed bug with today's transactions which didn't accepted into result on the shellview (AGAIN!)

--- Keeper/Models/TwoSelectorsModel.cs
+++ Keeper/Models/TwoSelectorsModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Windows;
 using Caliburn.Micro;
 using Keeper.DomainModel;
+using Keeper.Utils.Common;
 
 namespace Keeper.ViewModels.Shell
 {
@@ -20,7 +21,7 @@ namespace Keeper.ViewModels.Shell
 
     public DateTime TranslatedDate
     {
-      get { return _translatedDate; }
+      get { return new DayProcessor(_translatedDate).AfterThisDay(); }
       set 
       { 
         _translatedDate = value;
