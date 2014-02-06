ae19cffc1d90498dec54ef75ba240eeb45af33d7
Leonid <Leonid@opx-lmarholin>
2014-02-06 10:48:58 +0300

    class Ini, json embedded serializer is used

---
+++ Keeper/DomainModel/Ini.cs
@@ -0,0 +1,66 @@
+﻿using System.Composition;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using Keeper.Utils.Common;
+
+namespace Keeper.DomainModel
+{
+  [DataContract]
+  class MonthlyTraffic
+  {
+    [DataMember]
+    internal decimal SalaryCard { get; set; }
+    internal decimal SalaryEnvelope { get; set; }
+  }
+
+  [DataContract]
+  public class Ini
+  {
+    [DataMember]
+    internal decimal SalaryFull { get; set; }
+    [DataMember]
+    internal MonthlyTraffic MonthlyTraffic { get; set; }
+
+    public Ini()
+    {
+      MonthlyTraffic = new MonthlyTraffic();
+    }
+  }
+
+  [Export]
+  [Shared]
+  public class IniProvider
+  {
+    [Export]
+    public Ini MyIni { get; set; }
+
+    private readonly IMySettings _mySettings;
+    private readonly string _filename;
+
+    [ImportingConstructor]
+    public IniProvider(IMySettings mySettings)
+    {
+      _mySettings = mySettings;
+      _filename = Path.Combine((string)_mySettings.GetSetting("DbPath"), (string)_mySettings.GetSetting("IniFile"));
+
+      MyIni = ReadIni() ?? new Ini();
+    }
+
+    public void WriteIni()
+    {
+      var stream = new FileStream(_filename, FileMode.Create);
+      var jsonSerializer = new DataContractJsonSerializer(typeof(Ini));
+      jsonSerializer.WriteObject(stream, MyIni);
+    }
+
+    public Ini ReadIni()
+    {
+      if (!File.Exists(_filename)) return null;
+      var stream = new FileStream(_filename, FileMode.Open);
+      var jsonSerializer = new DataContractJsonSerializer(typeof(Ini));
+      return (Ini)jsonSerializer.ReadObject(stream);
+    }
+
+  }
+}
