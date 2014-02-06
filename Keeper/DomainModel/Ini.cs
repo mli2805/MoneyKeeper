e73343eefb6275a8663c36556bc279635dcb8a93
Leonid <Leonid@opx-lmarholin>
2014-02-06 16:39:24 +0300

    analysis collects estimated salary and apartments rent

--- Keeper/DomainModel/Ini.cs
+++ Keeper/DomainModel/Ini.cs
@@ -3,39 +3,24 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using Keeper.Utils.Common;
-using Keeper.Utils.Rates;
 
 namespace Keeper.DomainModel
 {
   [DataContract]
   public class MonthlyTraffic
   {
-    private readonly RateExtractor _rateExtractor;
-
-    [DataMember]
-    public decimal SalaryFull { get; set; }
-    [DataMember]
-    public decimal SalaryCard { get; set; }
-    public decimal SalaryEnvelope { get { return SalaryFull - SalaryCard/(decimal)_rateExtractor.GetLastRate(CurrencyCodes.BYR); } }
-
-    [ImportingConstructor]
-    public MonthlyTraffic(RateExtractor rateExtractor)
-    {
-      _rateExtractor = rateExtractor;
-    }
+    [DataMember] public decimal SalaryFull { get; set; }
+    [DataMember] public decimal SalaryCard { get; set; }
+    [DataMember] public decimal ApartmentsToLet { get; set; }
+    [DataMember] public decimal GskPayment { get; set; }
   }
 
   [DataContract]
   public class Ini
   {
-    [DataMember]
-    public MonthlyTraffic MonthlyTraffic { get; set; }
-
-    [DataMember]
-    public int GskPaymentAlarmDay { get; set; }
-
-    [DataMember]
-    public int UtilityPaymentsAlarmDay { get; set; }
+    [DataMember] public MonthlyTraffic MonthlyTraffic { get; set; }
+    [DataMember] public int GskPaymentAlarmDay { get; set; }
+    [DataMember] public int UtilityPaymentsAlarmDay { get; set; }
   }
 
   [Export]
