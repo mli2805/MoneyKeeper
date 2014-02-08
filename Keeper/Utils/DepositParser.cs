64220607b2a634ce41cc2ab9f362e0f8080cc503
mli2805 <mli2805@gmail.com>
2014-02-08 23:59:53 +0300

    Parser and Reporter are separated from Deposit

---
+++ Keeper/Utils/DepositParser.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Composition;
+using System.Globalization;
+using System.Linq;
+using Caliburn.Micro;
+using Keeper.DomainModel;
+using Keeper.Utils.Balances;
+using Keeper.Utils.Rates;
+
+namespace Keeper.Utils
+{
+  [Export]
+  public class DepositParser : PropertyChangedBase
+  {
+    private readonly KeeperDb _db;
+    private readonly RateExtractor _rateExtractor;
+    private readonly BalanceCalculator _balanceCalculator;
+
+    [ImportingConstructor]
+    public DepositParser(KeeperDb db, RateExtractor rateExtractor, BalanceCalculator balanceCalculator)
+    {
+      _db = db;
+      _rateExtractor = rateExtractor;
+      _balanceCalculator = balanceCalculator;
+    }
+
+    public Deposit Analyze(Account account)
+    {
+      return CollectInfo(new Deposit{Account = account});
+    }
+
+    private Deposit CollectInfo(Deposit deposit)
+    {
+      ExtractInfoFromName(deposit);
+      SelectTransactions(deposit);
+      if (deposit.Transactions.Count == 0)
+      {
+        deposit.State = DepositStates.Пустой;
+        return deposit;
+      }
+      deposit.MainCurrency = deposit.Transactions.First().Currency;
+      Calculate(deposit);
+//      if (deposit.Finish > DateTime.Today) ForecastProfit(deposit);
+      return deposit;
+    }
+
+    /// <summary>
+    /// из предположения, что обратные слэши только в датах, и даты с обеих сторон имеют пробелы
+    /// </summary>
+    private void ExtractInfoFromName(Deposit deposit)
+    {
+      var s = deposit.Account.Name;
+      var p = s.IndexOf('/');
+      var n = s.IndexOf(' ', p);
+      deposit.Start = Convert.ToDateTime(s.Substring(p - 2, n - p + 2), new CultureInfo("ru-RU"));
+      p = s.IndexOf('/', n);
+      n = s.IndexOf(' ', p);
+      deposit.Finish = Convert.ToDateTime(s.Substring(p - 2, n - p + 2), new CultureInfo("ru-RU"));
+      p = s.IndexOf('%', n);
+      deposit.DepositRate = Convert.ToDecimal(s.Substring(n, p - n));
+    }
+
+    private void SelectTransactions(Deposit deposit)
+    {
+      deposit.Transactions = (from transaction in _db.Transactions
+                              where transaction.Debet == deposit.Account || transaction.Credit == deposit.Account
+                      orderby transaction.Timestamp
+                      select transaction).ToList();
+    }
+
+    private void Calculate(Deposit deposit)
+    {
+      deposit.CurrentBalance = _balanceCalculator.GetBalanceInCurrency(deposit.Account,
+                                                                   new Period(new DateTime(0), DateTime.Today),
+                                                                   deposit.MainCurrency);
+      if (deposit.CurrentBalance == 0)
+        deposit.State = DepositStates.Закрыт;
+      else
+        deposit.State = deposit.Finish < DateTime.Today ? DepositStates.Просрочен : DepositStates.Открыт;
+
+      deposit.Profit = 0;
+      decimal balanceAfterTransaction = 0;
+      foreach (var transaction in deposit.Transactions)
+      {
+        var rate = transaction.Currency != CurrencyCodes.USD
+                     ? _rateExtractor.GetRate(transaction.Currency, transaction.Timestamp)
+                     : 1.0;
+
+        if (transaction.Credit == deposit.Account)
+        {
+          if (transaction.Operation == OperationType.Перенос)
+          {
+            if (balanceAfterTransaction == 0)
+              deposit.StartAmount = transaction.Amount;
+            else
+              deposit.AdditionalAmounts += transaction.Amount;
+            deposit.Profit = deposit.Profit - transaction.Amount / (decimal)rate;
+          }
+
+          balanceAfterTransaction += transaction.Amount;
+        }
+        else
+        {
+          deposit.Profit = deposit.Profit + transaction.Amount / (decimal)rate;
+          balanceAfterTransaction -= transaction.Amount;
+        }
+
+      }
+
+      if (deposit.CurrentBalance != 0)
+      {
+        var todayRate = deposit.MainCurrency != CurrencyCodes.USD ? _rateExtractor.GetLastRate(deposit.MainCurrency) : 1.0;
+        deposit.Profit += deposit.CurrentBalance / (decimal)todayRate;
+      }
+      deposit.Forecast = deposit.CurrentBalance * deposit.DepositRate / 100 * (deposit.Finish - deposit.Start).Days / 365;
+    }
+  }
+}
---
+++ Keeper/Utils/DepositReporter.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Composition;
+using System.Linq;
+using Keeper.DomainModel;
+using Keeper.Utils.Rates;
+
+namespace Keeper.Utils
+{
+  [Export]
+  public class DepositReporter
+  {
+    private readonly RateExtractor _rateExtractor;
+    private ObservableCollection<string> Report { get; set; }
+
+    [ImportingConstructor]
+    public DepositReporter(RateExtractor rateExtractor)
+    {
+      _rateExtractor = rateExtractor;
+      Report = new ObservableCollection<string>();
+    }
+
+    public ObservableCollection<string> BuildReport(Deposit deposit)
+    {
+      BuildReportHead(deposit);
+      BuildReportBody(deposit);
+      BuildReportFoot(deposit);
+      return Report;
+    }
+
+    private void BuildReportHead(Deposit deposit)
+    {
+      if (deposit.CurrentBalance == 0) Report.Add("Депозит закрыт. Остаток 0.\n");
+      else
+      {
+        Report.Add(deposit.Finish < DateTime.Today ? "!!! Срок депозита истек !!!\n" : "Действующий депозит.\n");
+        var balanceString = deposit.MainCurrency != CurrencyCodes.USD
+                              ? String.Format("{0:#,0} {2}  ($ {1:#,0} )",
+                                              deposit.CurrentBalance,
+                                              deposit.CurrentBalance /
+                                              (decimal)_rateExtractor.GetLastRate(deposit.MainCurrency),
+                                              deposit.MainCurrency.ToString().ToLower())
+                              : String.Format("{0:#,0} usd", deposit.CurrentBalance);
+        Report.Add(String.Format(" Остаток на {0:dd/MM/yyyy} составляет {1} \n", DateTime.Today, balanceString));
+      }
+      Report.Add("                             Расход                          Доход ");
+    }
+
+    private void BuildReportBody(Deposit deposit)
+    {
+      var isFirst = true;
+      foreach (var transaction in deposit.Transactions)
+      {
+        string comment;
+        if (transaction.Credit == deposit.Account)
+        {
+          if (transaction.Operation == OperationType.Перенос)
+            comment = isFirst ? "открытие депозита" : "доп взнос";
+          else comment = "начисление процентов";
+        }
+        else
+          comment = (deposit.CurrentBalance == 0 && transaction == deposit.Transactions.Last()) ?
+                             "закрытие депозита" : "частичное снятие";
+
+        Report.Add(String.Format("{0}     {1}", TransactionToLineInReport(transaction, deposit), comment));
+        isFirst = false;
+      }
+    }
+
+    private void BuildReportFoot(Deposit deposit)
+    {
+      Report.Add(String.Format("\nДоход по депозиту {0:#,0} usd \n", deposit.Profit));
+      Report.Add(ForecastProfit(deposit));
+    }
+
+    private string TransactionToLineInReport(Transaction transaction, Deposit deposit)
+    {
+      var s = transaction.Timestamp.ToString("dd/MM/yyyy");
+
+      var sum = transaction.Currency != CurrencyCodes.USD ?
+        String.Format("{0:#,0} {1}  ($ {2:#,0} )",
+         transaction.Amount, transaction.Currency.ToString().ToLower(), transaction.Amount / (decimal)_rateExtractor.GetRate(transaction.Currency, transaction.Timestamp)) :
+        String.Format("{0:#,0} usd", transaction.Amount);
+      if (transaction.Debet == deposit.Account) s = s + "   " + sum;
+      else s = s + "                                           " + sum;
+
+      return s;
+    }
+
+    private string ForecastProfit(Deposit deposit)
+    {
+      if (deposit.CurrentBalance == 0) return "";
+
+      var forecastInUsd = deposit.Forecast;
+
+      if (deposit.MainCurrency != CurrencyCodes.USD)
+      {
+        var todayRate = _rateExtractor.GetLastRate(deposit.MainCurrency);
+        forecastInUsd = forecastInUsd / (decimal)todayRate;
+      }
+      return String.Format("Прогноз по депозиту {0:#,0} usd", forecastInUsd);
+    }
+
+  }
+}
