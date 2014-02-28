ca8952f0d95ed1eba4935b2c960b3dd509f7b461
Leonid <Leonid@opx-lmarholin>
2014-02-28 10:57:55 +0300

    deposit rates form is ready

--- Keeper/ViewModels/DepositRatesViewModel.cs
+++ Keeper/ViewModels/DepositRatesViewModel.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using Caliburn.Micro;
 using Keeper.DomainModel;
 
@@ -11,16 +10,48 @@ namespace Keeper.ViewModels
 {
   class DepositRatesViewModel : Screen
   {
+    public DateTime NewDate { get; set; }
+
     public ObservableCollection<DepositRateLine> Rows { get; set; }
 
     public DepositRatesViewModel(Deposit deposit)
     {
+      NewDate = DateTime.Today;
+      if (deposit.DepositRateLines == null) deposit.DepositRateLines = new ObservableCollection<DepositRateLine>();
       Rows = deposit.DepositRateLines;
-      if (Rows == null)
+      if (Rows.Count == 0)
+        Rows.Add(new DepositRateLine { DateFrom = deposit.StartDate, DateTo = deposit.FinishDate, AmountFrom = 0, AmountTo = 999999999999, Rate = deposit.DepositRate });
+    }
+
+    public void AddLine()
+    {
+      if (Rows.Count == 0) return;
+      var lastLine = Rows[Rows.Count - 1];
+      var newLine = new DepositRateLine
+                      {
+                        DateFrom = lastLine.DateFrom,
+                        AmountFrom = lastLine.AmountTo + 1,
+                        AmountTo = 999999999999,
+                        Rate = lastLine.Rate + 1
+                      };
+      Rows.Add(newLine);
+    }
+
+    public void RepeatDay()
+    {
+      if (Rows.Count == 0) return;
+      var lastDate = Rows[Rows.Count - 1].DateFrom;
+
+      var lastDateLines = Rows.Where(depositRateLine => depositRateLine.DateFrom == lastDate).ToList();
+
+      foreach (var line in lastDateLines)
       {
-        Rows = new ObservableCollection<DepositRateLine>();
-        Rows.Add(new DepositRateLine{AmountFrom = 0, AmountTo = Decimal.MaxValue, DateFrom = deposit.StartDate, DateTo = deposit.FinishDate, Rate = deposit.DepositRate});
+        Rows.Add(new DepositRateLine{DateFrom = NewDate, AmountFrom = line.AmountFrom, AmountTo = line.AmountTo, Rate = line.Rate});
       }
     }
+
+    public void Close(){TryClose(true);}
+
+
   }
 }
