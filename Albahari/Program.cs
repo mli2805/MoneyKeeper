6fe6b007b355a8bb9f3d818fe1335d019cccd953
Leonid <Leonid@opx-lmarholin>
2014-03-07 16:21:24 +0300

    little funny example for lambda

---
+++ Albahari/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Albahari
+{
+  class Program
+  {
+    static void Main()
+    {
+      Func<int> natural = Natural();
+      Console.WriteLine(natural());
+      Console.WriteLine(natural());
+
+      Console.ReadKey();
+    }
+
+    static Func<int> Natural()
+    {
+      int seed = 0;
+      return () => seed++;
+    }
+
+  
+  }
+}
