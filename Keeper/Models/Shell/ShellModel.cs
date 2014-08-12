3d0ac27e2af47b37ba3df065a37e18513474136b
mli2805 <mli2805@gmail.com>
2014-08-12 10:02:31 +0300

    implemented possibility to see not balance but saldo of my accounts on main window

--- Keeper/Models/Shell/ShellModel.cs
+++ Keeper/Models/Shell/ShellModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Composition;
+using System.Security.AccessControl;
 using System.Windows;
 using Keeper.DomainModel;
 using Keeper.Utils.Balances;
@@ -7,129 +8,132 @@ using Keeper.ViewModels.Shell;
 
 namespace Keeper.Models.Shell
 {
-  [Export]
-  [Shared]
-  public class ShellModel
-  {
-    private readonly BalancesForShellCalculator _balancesForShellCalculator;
-
-    public MainMenuModel MyMainMenuModel { get; set; }
-    public AccountForestModel MyForestModel { get; set; }
-    public BalanceListModel MyBalanceListModel { get; set; }
-    public TwoSelectorsModel MyTwoSelectorsModel { get; set; }
-    public StatusBarModel MyStatusBarModel { get; set; }
-
-    [ImportingConstructor]
-    public ShellModel(BalancesForShellCalculator balancesForShellCalculator)
+    [Export]
+    [Shared]
+    public class ShellModel
     {
-      _balancesForShellCalculator = balancesForShellCalculator;
+        private readonly BalancesForShellCalculator _balancesForShellCalculator;
 
-      MyMainMenuModel = new MainMenuModel();
-      MyMainMenuModel.PropertyChanged += MyMainMenuModelPropertyChanged;
-      MyForestModel = new AccountForestModel();
-      MyForestModel.PropertyChanged += MyForestModelPropertyChanged;
-      MyBalanceListModel = new BalanceListModel();
-      MyTwoSelectorsModel = new TwoSelectorsModel();
-      MyTwoSelectorsModel.PropertyChanged += MyTwoSelectorsModelPropertyChanged;
-      MyStatusBarModel = new StatusBarModel();
-    }
+        public MainMenuModel MyMainMenuModel { get; set; }
+        public AccountForestModel MyForestModel { get; set; }
+        public BalanceListModel MyBalanceListModel { get; set; }
+        public TwoSelectorsModel MyTwoSelectorsModel { get; set; }
+        public StatusBarModel MyStatusBarModel { get; set; }
 
-    void MyMainMenuModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-      if (e.PropertyName == "Action")
-      {
-        switch (MyMainMenuModel.Action)
+        [ImportingConstructor]
+        public ShellModel(BalancesForShellCalculator balancesForShellCalculator)
         {
-          case Actions.Idle:
-            MyStatusBarModel.Item0 = "Готово";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
-            break;
+            _balancesForShellCalculator = balancesForShellCalculator;
 
-          case Actions.InputTransactions:
-            MyStatusBarModel.Item0 = "Ввод транзакций";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
-            break;
-          case Actions.InputRates:
-            MyStatusBarModel.Item0 = "Ввод курсов валют";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
-            break;
-          case Actions.InputAssociates:
-            MyStatusBarModel.Item0 = "Ввод ассоциаций";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
-            break;
-          case Actions.ShowAnalisys:
-            MyStatusBarModel.Item0 = "Анализ месяца";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
-            break;
-
-          case Actions.SaveDatabase:
-            MyStatusBarModel.Item0 = "Сохранение данных на диск...";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
-            break;
-          case Actions.LoadDatabase:
-            MyStatusBarModel.Item0 = "Загрузка данных с диска...";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
-            break;
-          case Actions.CleanDatabase:
-            MyStatusBarModel.Item0 = "Очистка БД...";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
-            break;
-          case Actions.RemoveIdenticalBackups:
-            MyStatusBarModel.Item0 = "Удаление идентичных резервных копий...";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
-            break;
-          case Actions.PrepareExit:
-            MyStatusBarModel.Item0 = "Идет завершение программы...";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
-            break;
-
-          case Actions.RefreshBalanceList:
-            RefreshBalanceListAccordinglyDatesInSelector();
-            MyStatusBarModel.Item0 = "Готово";
-            MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
-            break;
-
-          default: return;
+            MyMainMenuModel = new MainMenuModel();
+            MyMainMenuModel.PropertyChanged += MyMainMenuModelPropertyChanged;
+            MyForestModel = new AccountForestModel();
+            MyForestModel.PropertyChanged += MyForestModelPropertyChanged;
+            MyBalanceListModel = new BalanceListModel();
+            MyTwoSelectorsModel = new TwoSelectorsModel();
+            MyTwoSelectorsModel.PropertyChanged += MyTwoSelectorsModelPropertyChanged;
+            MyStatusBarModel = new StatusBarModel();
         }
-      }
-    }
 
-    private void RefreshBalanceListAccordinglyDatesInSelector()
-    {
-      if (MyTwoSelectorsModel.IsPeriodMode)
-        MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
-          MyForestModel.SelectedAccount, MyTwoSelectorsModel.TranslatedPeriod, MyBalanceListModel.BalanceList));
-      else
-        MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
-          MyForestModel.SelectedAccount, new Period(new DateTime(0), MyTwoSelectorsModel.TranslatedDate),
-          MyBalanceListModel.BalanceList));
-    }
-
-    void MyTwoSelectorsModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-      RefreshBalanceListAccordinglyDatesInSelector();
-    }
-
-    void MyForestModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-      if (e.PropertyName == "SelectedAccount")
-      {
-        MyBalanceListModel.Caption = MyForestModel.SelectedAccount.Name;
-        if (MyTwoSelectorsModel.IsPeriodMode)
+        void MyMainMenuModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-          MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
-            MyForestModel.SelectedAccount, MyTwoSelectorsModel.TranslatedPeriod, MyBalanceListModel.BalanceList));
+            if (e.PropertyName == "Action")
+            {
+                switch (MyMainMenuModel.Action)
+                {
+                    case Actions.Idle:
+                        MyStatusBarModel.Item0 = "Готово";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
+                        break;
+
+                    case Actions.InputTransactions:
+                        MyStatusBarModel.Item0 = "Ввод транзакций";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
+                        break;
+                    case Actions.InputRates:
+                        MyStatusBarModel.Item0 = "Ввод курсов валют";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
+                        break;
+                    case Actions.InputAssociates:
+                        MyStatusBarModel.Item0 = "Ввод ассоциаций";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
+                        break;
+                    case Actions.ShowAnalisys:
+                        MyStatusBarModel.Item0 = "Анализ месяца";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
+                        break;
+
+                    case Actions.SaveDatabase:
+                        MyStatusBarModel.Item0 = "Сохранение данных на диск...";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
+                        break;
+                    case Actions.LoadDatabase:
+                        MyStatusBarModel.Item0 = "Загрузка данных с диска...";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
+                        break;
+                    case Actions.CleanDatabase:
+                        MyStatusBarModel.Item0 = "Очистка БД...";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
+                        break;
+                    case Actions.RemoveIdenticalBackups:
+                        MyStatusBarModel.Item0 = "Удаление идентичных резервных копий...";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
+                        break;
+                    case Actions.PrepareExit:
+                        MyStatusBarModel.Item0 = "Идет завершение программы...";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Visible;
+                        break;
+
+                    case Actions.RefreshBalanceList:
+                        RefreshBalanceListAccordinglyDatesInSelector();
+                        MyStatusBarModel.Item0 = "Готово";
+                        MyStatusBarModel.ProgressBarVisibility = Visibility.Collapsed;
+                        break;
+
+                    default: return;
+                }
+            }
         }
-        else
+
+        private void RefreshBalanceListAccordinglyDatesInSelector()
         {
-          MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
-            MyForestModel.SelectedAccount, new Period(new DateTime(0), MyTwoSelectorsModel.TranslatedDate), MyBalanceListModel.BalanceList));
+            if (MyTwoSelectorsModel.IsPeriodMode)
+                MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
+                  MyForestModel.SelectedAccount, MyTwoSelectorsModel.TranslatedPeriod, MyBalanceListModel.BalanceList));
+            else
+                MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
+                  MyForestModel.SelectedAccount, new Period(new DateTime(0), MyTwoSelectorsModel.TranslatedDate),
+                  MyBalanceListModel.BalanceList));
         }
-      }
 
-      if (e.PropertyName == "OpenedAccountPage")
-        MyTwoSelectorsModel.IsPeriodMode = MyForestModel.OpenedAccountPage != 0;
+        void MyTwoSelectorsModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "ChangeControlTypeTranslatedEvent") 
+                MyTwoSelectorsModel.IsPeriodMode = !MyTwoSelectorsModel.IsPeriodMode;
+            else 
+                RefreshBalanceListAccordinglyDatesInSelector();
+        }
+
+        void MyForestModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SelectedAccount")
+            {
+                MyBalanceListModel.Caption = MyForestModel.SelectedAccount.Name;
+                if (MyTwoSelectorsModel.IsPeriodMode)
+                {
+                    MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
+                      MyForestModel.SelectedAccount, MyTwoSelectorsModel.TranslatedPeriod, MyBalanceListModel.BalanceList));
+                }
+                else
+                {
+                    MyBalanceListModel.AccountBalanceInUsd = String.Format("{0:#,#} usd", _balancesForShellCalculator.FillListForShellView(
+                      MyForestModel.SelectedAccount, new Period(new DateTime(0), MyTwoSelectorsModel.TranslatedDate), MyBalanceListModel.BalanceList));
+                }
+            }
+
+            if (e.PropertyName == "OpenedAccountPage")
+                MyTwoSelectorsModel.IsPeriodMode = MyForestModel.OpenedAccountPage != 0;
+        }
+
     }
-
-  }
 }
--- Keeper/Views/Shell/TwoSelectorsView.xaml.cs
+++ Keeper/Views/Shell/TwoSelectorsView.xaml.cs
@@ -1,38 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Keeper.Views.Shell
 {
-  /// <summary>
-  /// Interaction logic for TwoSelectorsView.xaml
-  /// </summary>
-  public partial class TwoSelectorsView : UserControl
-  {
-    public TwoSelectorsView()
+    /// <summary>
+    /// Interaction logic for TwoSelectorsView.xaml
+    /// </summary>
+    public partial class TwoSelectorsView : UserControl
     {
-      InitializeComponent();
-    }
+        public TwoSelectorsView()
+        {
+            InitializeComponent();
+        }
 
-    public void Connect(int connectionId, object target)
-    {
-      throw new NotImplementedException();
-    }
-
-    private void ShellViewPeriodSelectControl_Loaded_1(object sender, RoutedEventArgs e)
-    {
+        public void Connect(int connectionId, object target)
+        {
+            throw new NotImplementedException();
+        }
 
     }
-  }
 }
