3d8e666e0a76bdc4f18ff6c8676ff9b6e49328ec
mli2805 <mli2805@gmail.com>
2014-11-10 16:31:40 +0300

    little renaming

---
+++ Keeper.UnitTests/ByFunctional/EvaluatingBalances/TestArticleBalanceCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FluentAssertions;
+using Keeper.DomainModel;
+using Keeper.Utils.Accounts;
+using Keeper.Utils.Balances;
+using Keeper.Utils.DbInputOutput.CompositeTasks;
+using Keeper.Utils.DbInputOutput.TxtTasks;
+using NUnit.Framework;
+
+namespace Keeper.UnitTests.ByFunctional.EvaluatingBalances
+{
+    [TestFixture]
+    public sealed class TestArticleBalanceCalculator
+    {
+        DbLoadResult _loadResult;
+        ArticleBalanceCalculator _underTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loadResult = new DbFromTxtLoader(new DbClassesInstanceParser(),
+            new AccountTreeStraightener()).LoadDbFromTxt(Path.GetFullPath(@"TestDb\forBalanceEvaluationTest"));
+
+            if (_loadResult.Code != 0) MessageBox.Show("Database wasn't loaded properly!");
+
+            _underTest = new ArticleBalanceCalculator(_loadResult.Db);
+        }
+
+        public void GetArticleBalanceInUsdPlus_Should_Evaluate()
+        {
+            const decimal expectedSum = 57;
+            var expectedTransactions = new List<string>{ "", ""};
+
+            var testedArticle = new Account("Одежда");
+            _underTest.GetArticleBalanceInUsdPlus(testedArticle,
+                                                  new Period(new DateTime(2014, 10, 30, 9, 5, 0), new DateTime(2014, 11, 3, 0, 0, 0)),
+                                                  expectedTransactions).ShouldBeEquivalentTo(expectedSum);
+            
+        }
+
+    }
+}
---
+++ Keeper/ByFunctional/EvaluatingBalances/ArticleBalanceCalculator.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using Keeper.DomainModel;
+
+namespace Keeper.Utils.Balances
+{
+    [Export]
+    public class ArticleBalanceCalculator
+    {
+        private readonly KeeperDb _db;
+
+        [ImportingConstructor]
+        public ArticleBalanceCalculator(KeeperDb db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// возвращает остаток по —“ј“№≈ (доходов / расходов) переведенный в доллары и дополнительно
+        /// список субсчетов если есть или список операций по счету
+        /// </summary>
+        /// <param name="article">стать€ (доходов / расходов)</param>
+        /// <param name="period">временной интервал</param>
+        /// <param name="transactions">список</param>
+        /// <returns></returns>
+        public decimal GetArticleBalanceInUsdPlus(Account article, Period period, List<string> transactions)
+        {
+            var transactionsWithRates = (from t in _db.Transactions
+                                         where t.Article != null && t.Article.Is(article.Name) && period.ContainsButTimeNotChecking(t.Timestamp)
+                                         join r in _db.CurrencyRates on new { t.Timestamp.Date, t.Currency } equals new { r.BankDay.Date, r.Currency } into g
+                                         from rate in g.DefaultIfEmpty()
+                                         select new
+                                         {
+                                             t.Timestamp,
+                                             AmountInUsd = rate != null ? t.Amount / (decimal)rate.Rate : t.Amount,
+                                             t.Comment
+                                         }).ToList();
+
+            var am = transactionsWithRates.Sum(t => t.AmountInUsd);
+
+            if (am != 0 && article.Children.Count == 0)
+            {
+                transactions.Clear();
+                for (var i = 0; i < transactionsWithRates.Count(); i++)
+                {
+                    transactions.Add(string.Format("  {0:dd/MM/yyyy} ${1:#,0} {2}", transactionsWithRates[i].Timestamp, transactionsWithRates[i].AmountInUsd, transactionsWithRates[i].Comment.Trim()));
+                }
+            }
+
+            return am;
+        }
+
+    }
+}
