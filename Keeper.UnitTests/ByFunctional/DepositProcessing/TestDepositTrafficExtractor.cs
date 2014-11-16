f4aacb541209d45e55c983f6acce28d08e38881d
mli2805 <mli2805@gmail.com>
2014-11-16 09:59:33 +0300

    test for deposit traffic extractor

--- Keeper.UnitTests/ByFunctional/DepositProcessing/TestDepositTrafficExtractor.cs
+++ Keeper.UnitTests/ByFunctional/DepositProcessing/TestDepositTrafficExtractor.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using Keeper.ByFunctional.AccountEditing;
 using Keeper.ByFunctional.DepositProcessing;
+using Keeper.DomainModel;
 using Keeper.DomainModel.Deposit;
 using Keeper.Utils.DbInputOutput.CompositeTasks;
 using Keeper.Utils.DbInputOutput.TxtTasks;
@@ -12,10 +15,10 @@ using FluentAssertions;
 namespace Keeper.UnitTests.ByFunctional.DepositProcessing
 {
     [TestFixture]
-    public sealed class TestDepositTrafficEvaluator
+    public sealed class TestDepositTrafficExtractor
     {
         DbLoadResult _loadResult;
-        DepositTrafficEvaluator _underTest;
+        DepositTrafficExtractor _underTest;
 
         [SetUp]
         public void SetUp()
@@ -24,15 +27,20 @@ namespace Keeper.UnitTests.ByFunctional.DepositProcessing
                                                LoadDbFromTxt(Path.GetFullPath(@"TestDb\forDepositCalculationTest"));
 
             if (_loadResult.Code != 0) MessageBox.Show("Database wasn't loaded properly!");
-            _underTest = new DepositTrafficEvaluator(new RateExtractor(_loadResult.Db));
-            
+            _underTest = new DepositTrafficExtractor(_loadResult.Db);
+
         }
 
         [Test]
         public void EvaluateTraffic_Should_Evaluate()
         {
-             var deposit = new Deposit();
-            _underTest.EvaluateTraffic(deposit).CalculationData.DailyTable.Count.Should().Be(3);
+            var ats = new AccountTreeStraightener();
+            var accounts = ats.Flatten(_loadResult.Db.Accounts);
+
+            var account = accounts.First(a => a.Name == "ВТБ Скарбонка2 8/08/2014 - 7/09/2015 33-31%");
+
+            var depositExtractor = new DepositTrafficExtractor(_loadResult.Db);
+            depositExtractor.ExtractTraffic(account).CalculationData.Traffic.Count.Should().Be(10);
         }
     }
 }
