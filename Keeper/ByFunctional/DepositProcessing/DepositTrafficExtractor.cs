3370bf236e8ef98096c1ea5b1951f59068c4a8ba
mli2805 <mli2805@gmail.com>
2015-09-01 22:06:11 +0300

    problems with currency exchange on deposit account

--- Keeper/ByFunctional/DepositProcessing/DepositTrafficExtractor.cs
+++ Keeper/ByFunctional/DepositProcessing/DepositTrafficExtractor.cs
@@ -1,7 +1,9 @@
 ﻿using System.Composition;
+using System.Collections.Generic;
 using System.Linq;
 using Keeper.DomainModel;
 using Keeper.DomainModel.Deposit;
+using Keeper.Utils.Rates;
 
 namespace Keeper.ByFunctional.DepositProcessing
 {
@@ -10,22 +12,25 @@ namespace Keeper.ByFunctional.DepositProcessing
     {
         private readonly KeeperDb _db;
         private Deposit _deposit;
+        private RateExtractor _rateExtractor;
 
         [ImportingConstructor]
-        public DepositTrafficExtractor(KeeperDb db)
+        public DepositTrafficExtractor(KeeperDb db, RateExtractor rateExtractor)
         {
             _db = db;
+            _rateExtractor = rateExtractor;
         }
 
-        public Deposit ExtractTraffic(Account account)
+        public Deposit ExtractTraffic(Account account) // используется при месячном анализе
         {
             _deposit = account.Deposit;
             _deposit.CalculationData = new DepositCalculationData();
-            ExtractViaSqlRequest();
+            ExtractViaSqlRequest();   // быстрый
+//            ExtractInCombainedMode();
             return _deposit;
         }
 
-        private void ExtractViaSqlRequest()
+        private void ExtractViaSqlRequest() // проблемы с операциями обмена (должны отобразиться в обе стороны)
         {
             _deposit.CalculationData.Traffic = 
                 (from t in _db.Transactions
@@ -42,6 +47,64 @@ namespace Keeper.ByFunctional.DepositProcessing
                      AmountInUsd = rate != null ? t.Amount / (decimal)rate.Rate : t.Amount,
                      TransactionType = GetDepositOperationType(t, _deposit.ParentAccount)
                  }).ToList();
+        }
+
+        private void ExtractInCombainedMode()
+        {
+            var trs = from t in _db.Transactions
+                      where t.Debet == _deposit.ParentAccount || t.Credit == _deposit.ParentAccount
+                      select t;
+
+            _deposit.CalculationData.Traffic = new List<DepositTransaction>();
+            foreach (var tr in trs) 
+            {
+                if (tr.Operation == OperationType.Обмен)
+                    _deposit.CalculationData.Traffic.AddRange(ConvertExchangeTransactionToDepositTransaction(tr));
+                else
+                    _deposit.CalculationData.Traffic.Add(ConvertCommonTransactionToDepositTransaction(tr));
+
+            }
+        }
+
+        private DepositTransaction ConvertCommonTransactionToDepositTransaction(Transaction t)
+        {
+            var result = new DepositTransaction()
+                {
+                    Amount = t.Amount,
+                    Timestamp = t.Timestamp,
+                    Currency = t.Currency,
+                    Comment = GetDepositOperationComment(t),
+                    AmountInUsd = _rateExtractor.GetUsdEquivalent(t.Amount, t.Currency, t.Timestamp),
+                    TransactionType = GetDepositOperationType(t, _deposit.ParentAccount)
+                };
+            return result;
+        }
+
+        private IEnumerable<DepositTransaction> ConvertExchangeTransactionToDepositTransaction(Transaction t)
+        {
+            var result = new List<DepositTransaction>();
+
+            result.Add(new DepositTransaction()
+            {
+                    Amount = t.Amount,
+                    Timestamp = t.Timestamp,
+                    Currency = t.Currency,
+                    Comment = GetDepositOperationComment(t),
+                    AmountInUsd = _rateExtractor.GetUsdEquivalent(t.Amount, t.Currency, t.Timestamp),
+                    TransactionType = DepositTransactionTypes.ОбменРасход
+            });
+
+            result.Add(new DepositTransaction()
+            {
+                Amount = t.Amount2,
+                Timestamp = t.Timestamp,
+                Currency = (CurrencyCodes)t.Currency2,
+                Comment = GetDepositOperationComment(t),
+                AmountInUsd = _rateExtractor.GetUsdEquivalent(t.Amount2, (CurrencyCodes)t.Currency2, t.Timestamp),
+                TransactionType = DepositTransactionTypes.ОбменДоход
+            });
+
+            return result;
         }
 
         private DepositTransactionTypes GetDepositOperationType(Transaction t, Account depositAccount)
