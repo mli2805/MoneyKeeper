851026015ebf5b2ed95a011ed1a08ab1cc488296
mli2805 <mli2805@gmail.com>
2015-09-03 09:49:47 +0300

    DataTemplateSelector is implemented for TransactionView

---
+++ Keeper/XamlExtensions/AccountTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Keeper.DomainModel;
+
+namespace Keeper.XamlExtensions
+{
+    class AccountTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate AmountStyleForByr { get; set; }
+        public DataTemplate AmountStyleForOthers { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (!(item is Transaction)) return null;
+            var tr = item as Transaction;
+            return tr.Currency == CurrencyCodes.BYR ? AmountStyleForByr : AmountStyleForOthers;
+        }
+    }
+}
