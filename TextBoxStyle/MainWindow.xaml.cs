e45ffbf3b0a84ab366e9ae0d3e0d96857a047ac6
mli2805 <mli2805@gmail.com>
2015-09-10 15:55:52 +0300

    test project for studying posibilities of text box styling

---
+++ TextBoxStyle/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TextBoxStyle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public decimal Amount { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            Amount = 3.04m;
+            MyListBox.Items.Add("Hello!");
+            MyListBox.Items.Add("Amount Text Box should contain number 3");
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SwitchStyle_Click(object sender, RoutedEventArgs e)
+        {
+            var template = (ControlTemplate)FindResource("MyControlTemplateWithFractional");
+            AmounTextBox.Template = template;
+        }
+    }
+}
