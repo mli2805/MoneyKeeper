19b0f987f10fa4f5c85e9bdc430a9e803ed2aa40
mli2805 <mli2805@gmail.com>
2015-10-31 21:45:51 +0300

    gaps in rates dictionary are filled with previous values

--- Keeper/DomainModel/CurrencyRatesAsDictionary.cs
+++ Keeper/DomainModel/CurrencyRatesAsDictionary.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Office.Interop.Excel;
 
 namespace Keeper.DomainModel
 {
@@ -7,18 +9,33 @@ namespace Keeper.DomainModel
     {
         public Dictionary<CurrencyCodes, Dictionary<DateTime, double>> Rates { get; set; }
 
-        public CurrencyRatesAsDictionary(IEnumerable<CurrencyRate> collection)
+        public CurrencyRatesAsDictionary(List<CurrencyRate> collection)
         {
             Rates = new Dictionary<CurrencyCodes, Dictionary<DateTime, double>>
             {
-                {CurrencyCodes.BYR, new Dictionary<DateTime, double>()},
-                {CurrencyCodes.EUR, new Dictionary<DateTime, double>()},
-                {CurrencyCodes.RUB, new Dictionary<DateTime, double>()}
+                {CurrencyCodes.BYR, ConvertOneCurrency(collection, CurrencyCodes.BYR)},
+                {CurrencyCodes.EUR, ConvertOneCurrency(collection, CurrencyCodes.EUR)},
+                {CurrencyCodes.RUB, ConvertOneCurrency(collection, CurrencyCodes.RUB)}
             };
-            foreach (var currencyRate in collection)
+        }
+
+        private Dictionary<DateTime, double> ConvertOneCurrency(IEnumerable<CurrencyRate> collection, CurrencyCodes currency)
+        {
+            var dictionary = new Dictionary<DateTime, double>();
+            var previousDate = new DateTime(2001, 12, 31);
+            var previousRate = 0.0;
+            foreach (var currentRate in collection.Where(t => t.Currency == currency).OrderBy(t => t.BankDay))
             {
-                Rates[currencyRate.Currency].Add(currencyRate.BankDay.Date, currencyRate.Rate);
+                while (previousDate.Date < currentRate.BankDay.Date)
+                {
+                    dictionary.Add(previousDate.Date, previousRate);
+                    previousDate = previousDate.AddDays(1);
+                }
+                dictionary.Add(currentRate.BankDay.Date, currentRate.Rate);
+                previousDate = currentRate.BankDay.AddDays(1);
+                previousRate = currentRate.Rate;
             }
+            return dictionary;
         }
 
         /// <summary>
