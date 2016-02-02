ce33fbdffd344129ca37accaab3a7eb99b9f7af8
mli2805 <mli2805@gmail.com>
2016-02-02 17:33:10 +0300

    functions for new transactions classes

---
+++ Keeper/ByFunctional/BalanceEvaluating/TrExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Keeper.DomainModel;
+using Keeper.DomainModel.Transactions;
+
+namespace Keeper.ByFunctional.BalanceEvaluating
+{
+    public static class TrExtensions
+    {
+        public static bool f(this TrBase transaction, Account account)
+        {
+            return true;
+        }
+
+    }
+}
