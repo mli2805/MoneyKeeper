3245e81b74c89340091d78ed01c7b75b19d1c0c8
mli2508n <mli2508n@localhost>
2016-03-01 16:26:01 +0300

    TranWithTags Clone

--- Keeper/DomainModel/Transactions/TranExtentions.cs
+++ Keeper/DomainModel/Transactions/TranExtentions.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Keeper.DomainModel.Transactions
 {
     public static class TranExtentions
     {
+        public static void CloneFrom(this TranWithTags tran, TranWithTags donor)
+        {
+            tran.Timestamp = donor.Timestamp;
+            tran.Operation = donor.Operation;
+            tran.MyAccount = donor.MyAccount;
+            tran.MySecondAccount = donor.MySecondAccount;
+            tran.Amount = donor.Amount;
+            tran.Currency = donor.Currency;
+            tran.AmountInReturn = donor.AmountInReturn;
+            tran.CurrencyInReturn = donor.CurrencyInReturn;
+            if (donor.Tags != null)
+            {
+                tran.Tags = new List<Account>();
+                foreach (var tag in donor.Tags)
+                {
+                    tran.Tags.Add(tag);
+                }
+            }
+            tran.Comment = donor.Comment;
+        }
         public static decimal AmountForAccount(this TranWithTags tran, Account account, CurrencyCodes? currency, DateTime upToDateTime)
         {
             return tran.Timestamp <= upToDateTime ? AmountForAccount(tran, account, currency) : 0;
