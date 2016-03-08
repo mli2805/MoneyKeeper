c920b9a6cc9fe9f0cea06008776109e90b0514ef
mli2805 <mli2805@localhost>
2016-03-08 18:59:51 +0300

    TestControl without binding errors

--- Keeper/Controls/TestControl.xaml.cs
+++ Keeper/Controls/TestControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Keeper.Controls
 {
@@ -23,6 +11,11 @@ namespace Keeper.Controls
         public TestControl()
         {
             InitializeComponent();
+        }
+
+        private void PressButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
