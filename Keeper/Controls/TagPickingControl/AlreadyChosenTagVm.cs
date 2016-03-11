6a2e18a798f19a96c9a4683d4505db15c22fc4b5
mli2805 <mli2805@gmail.com>
2016-03-11 07:58:02 +0300

    started new TagPickingControl. Doesn't work!!!

---
+++ Keeper/Controls/TagPickingControl/AlreadyChosenTagVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Keeper.DomainModel.WorkTypes;
+
+namespace Keeper.Controls.TagPickingControl
+{
+    class AlreadyChosenTagVm
+    {
+        public AccName Tag { get; set; }
+    }
+}
---
+++ Keeper/Controls/TagPickingControl/TagPicker.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Keeper.Controls.TagPickingControl
+{
+    /// <summary>
+    /// Interaction logic for TagPicker.xaml
+    /// </summary>
+    public partial class TagPicker : UserControl
+    {
+        public TagPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
