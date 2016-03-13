372faf4824a751ec92d8c3df191561c88c7c0a84
mli2805 <mli2805@localhost>
2016-03-13 19:37:36 +0300

    all elements on expense form

---
+++ Keeper/Controls/DatePickerWithTriangles.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Keeper.Controls
+{
+    /// <summary>
+    /// Interaction logic for DatePickerWithTriangles.xaml
+    /// </summary>
+    public partial class DatePickerWithTriangles : UserControl
+    {
+        public DatePickerWithTriangles()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonDecreaseOnClick(object sender, RoutedEventArgs e)
+        {
+            var dc = (DatePickerWithTrianglesVm) DataContext;
+            dc.SelectedDate = dc.SelectedDate.AddDays(-1);
+        }
+
+        private void ButtonIncreaseOnClick(object sender, RoutedEventArgs e)
+        {
+            var dc = (DatePickerWithTrianglesVm)DataContext;
+            dc.SelectedDate = dc.SelectedDate.AddDays(1);
+        }
+    }
+}
