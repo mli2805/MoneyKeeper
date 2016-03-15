17c3957afd90e34f5f99c37a56553c27a8350ab1
mli2805 <mli2805@localhost>
2016-03-15 22:04:27 +0300

    IncomeControl started

---
+++ Keeper/Controls/IncomeControlVm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Keeper.Controls.AccNameSelectionControl;
+using Keeper.Controls.TagPickingControl;
+using Keeper.DomainModel.Transactions;
+using Keeper.ViewModels.TransWithTags;
+
+namespace Keeper.Controls
+{
+    class IncomeControlVm
+    {
+        private readonly BalanceDuringTransactionHinter _balanceDuringTransactionHinter;
+
+        public TranWithTags TranInWork { get; set; }
+        public AccNameSelectorVm MyAccNameSelectorVm { get; set; }
+        public AmountInputControlVm MyAmountInputControlVm { get; set; }
+        public TagPickerVm MyTagPickerVm { get; set; }
+        public DatePickerWithTrianglesVm MyDatePickerVm { get; set; }
+
+        public string MyAccountBalance => _balanceDuringTransactionHinter.GetMyAccountBalance(TranInWork);
+        public string AmountInUsd => _balanceDuringTransactionHinter.GetAmountInUsd(TranInWork);
+
+        public IncomeControlVm(BalanceDuringTransactionHinter balanceDuringTransactionHinter)
+        {
+            _balanceDuringTransactionHinter = balanceDuringTransactionHinter;
+        }
+    }
+}
