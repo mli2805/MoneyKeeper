b747c903273cb84228a88a9b535355aa91656863
mli2805 <mli2805@gmail.com>
2016-04-22 16:47:25 +0300

    new tranwithtags is added into db

--- Keeper/Behaviors/DataGridBehaviors/ScrollToBottomOnLoadDatagridBehavior.cs
+++ Keeper/Behaviors/DataGridBehaviors/ScrollToBottomOnLoadDatagridBehavior.cs
@@ -1,20 +1,23 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interactivity;
 
 namespace Keeper.Behaviors.DataGridBehaviors
 {
-  class ScrollToBottomOnLoadDatagridBehavior : Behavior<DataGrid>
-  {
-    protected override void OnAttached() 
+    class ScrollToBottomOnLoadDatagridBehavior : Behavior<DataGrid>
     {
-      AssociatedObject.Loaded += AssociatedObjectOnLoaded; 
-    }
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += AssociatedObjectOnLoaded;
+            Console.WriteLine("ScrollToBottomOnLoadDatagridBehavior attached");
+        }
 
-    private void AssociatedObjectOnLoaded(object sender, RoutedEventArgs routedEventArgs)
-    {
-      if (AssociatedObject.Items.Count > 0)
-      AssociatedObject.ScrollIntoView(AssociatedObject.Items[AssociatedObject.Items.Count - 1]);
+        private void AssociatedObjectOnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            if (AssociatedObject.Items.Count > 0)
+                AssociatedObject.ScrollIntoView(AssociatedObject.Items[AssociatedObject.Items.Count - 1]);
+            Console.WriteLine("ScrollToBottomOnLoadDatagridBehavior works");
+        }
     }
-  }
 }
