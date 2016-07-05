6232d39d2ca1698a5f44c368caa45aa61da50cd0
mli2805 <mli2805@gmail.com>
2016-07-05 12:16:38 +0300

    exchange rates display format with 4 digits after point automatic denomination

--- Keeper/Utils/Rates/RateDefiner.cs
+++ Keeper/Utils/Rates/RateDefiner.cs
@@ -11,26 +11,36 @@ namespace Keeper.Utils.Rates
             if (currency1 == currency2) return "ошибка - одинаковая валюта";
 
             if (currency1 == CurrencyCodes.BYR)
-                return RateToString(amount1, amount2);
+                return RateToString(amount1, amount2, true);
             if (currency2 == CurrencyCodes.BYR)
-                return RateToString(amount2, amount1);
+                return RateToString(amount2, amount1, true);
+
+            if (currency1 == CurrencyCodes.BYN)
+                return RateToString(amount1, amount2, false);
+            if (currency2 == CurrencyCodes.BYN)
+                return RateToString(amount2, amount1, false);
 
             if (currency1 == CurrencyCodes.RUB)
-                return RateToString(amount1, amount2);
+                return RateToString(amount1, amount2, false);
             if (currency2 == CurrencyCodes.RUB)
-                return RateToString(amount2, amount1);
+                return RateToString(amount2, amount1, false);
 
             if (currency1 == CurrencyCodes.USD)
-                return RateToString(amount1, amount2);
+                return RateToString(amount1, amount2, false);
             if (currency2 == CurrencyCodes.USD)
-                return RateToString(amount2, amount1);
+                return RateToString(amount2, amount1, false);
 
-            return "EUR is the most expensive currency in my enum";
+            return "EUR is the most valuable currency in my enum";
         }
 
-        private static  string RateToString(decimal cheapCurrency, decimal expensiveCurrency)
+        private static  string RateToString(decimal amountInCheapCurrency, decimal amountInExpensiveCurrency, bool isByr)
         {
-            return expensiveCurrency != 0 ? String.Format("по курсу {0:#,0.####}", cheapCurrency / expensiveCurrency) : "";
+
+            return amountInExpensiveCurrency == 0
+                ? ""
+                : isByr
+                    ? String.Format("по курсу {0:#,0.####}", amountInCheapCurrency/amountInExpensiveCurrency)
+                    : String.Format("по курсу {0:#,0.0000}", amountInCheapCurrency/amountInExpensiveCurrency);
         }
 
     }
