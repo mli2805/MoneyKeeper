763e53b8ba7ba5e61e31b3859c04ed799034ad42
mli2805 <mli2805@gmail.com>
2016-09-02 16:05:14 +0300

    trans filter works

--- Keeper/ViewModels/TransWithTags/TransViewModel.cs
+++ Keeper/ViewModels/TransWithTags/TransViewModel.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Composition;
-using System.Linq;
-using System.Windows.Data;
 using Caliburn.Micro;
 using Keeper.DomainModel.DbTypes;
-using Keeper.DomainModel.Trans;
 
 namespace Keeper.ViewModels.TransWithTags
 {
     [Export]
     class TransViewModel : Screen
     {
+        private int _left;
         public int Left
         {
             get { return _left; }
@@ -22,55 +19,25 @@ namespace Keeper.ViewModels.TransWithTags
                 _filterViewModel?.PlaceIt(Left, Top, FilterViewWidth);
             }
         }
-
         public int Top { get; set; }
-        public int FilterViewWidth = 230;
-        public TransModel Model { get; set; } = new TransModel();
+        public int FilterViewWidth = 225;
+        public TransModel Model { get; set; }
         public TranEditActionsExecutor EditActionsExecutorHandler { get; set; } = new TranEditActionsExecutor();
         public TranLocateActionsExecutor LocateActionsExecutorHandler { get; set; } = new TranLocateActionsExecutor();
-        public bool IsCollectionChanged { get; set; }
+        public bool IsCollectionChanged => Model.IsCollectionChanged;
 
         private FilterViewModel _filterViewModel;
-        private int _left;
 
         [ImportingConstructor]
         public TransViewModel(KeeperDb db)
         {
-            Model.Db = db;
-
-            Model.Rows = WrapTransactions(Model.Db.TransWithTags);
-            Model.Rows.CollectionChanged += Rows_CollectionChanged;
-
-            Model.SortedRows = CollectionViewSource.GetDefaultView(Model.Rows);
-            Model.SortedRows.SortDescriptions.Add(new SortDescription("Tran.Timestamp", ListSortDirection.Ascending));
-
-            Model.SelectedTranWrappedForDatagrid = Model.Rows.OrderBy(t => t.Tran.Timestamp).Last();
-            Model.SelectedTranWrappedForDatagrid.IsSelected = true;
-
-            IsCollectionChanged = false;
+            Model = new TransModel(db);
             Top = 200;
-            Left = 400;
-        }
-
-        private void Rows_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            IsCollectionChanged = true;
-        }
-
-        private ObservableCollection<TranWrappedForDatagrid> WrapTransactions(ObservableCollection<TranWithTags> transactions)
-        {
-            var result = new ObservableCollection<TranWrappedForDatagrid>();
-            foreach (var tran in transactions)
-            {
-                result.Add(new TranWrappedForDatagrid() { Tran = tran });
-            }
-            return result;
+            Left = 700;
         }
         protected override void OnViewLoaded(object view)
         {
             DisplayName = "Transactions with tags";
-            Top = 200;
-            Left = 400;
         }
         public override void CanClose(Action<bool> callback)
         {
@@ -80,28 +47,33 @@ namespace Keeper.ViewModels.TransWithTags
         }
         public void ButtonFilter()
         {
+            var wm = new WindowManager();
+
             _filterViewModel = IoC.Get<FilterViewModel>();
             _filterViewModel.PlaceIt(Left, Top, FilterViewWidth);
-            var wm = new WindowManager();
             _filterViewModel.PropertyChanged += FilterViewModel_PropertyChanged;
+            _filterViewModel.FilterModel.PropertyChanged += FilterModel_PropertyChanged;
+
             wm.ShowWindow(_filterViewModel);
+        }
+
+        private void FilterModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Model.FilterModel = _filterViewModel.FilterModel;
         }
 
         private void FilterViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            ApplyFilter();
+            if (e.PropertyName == "IsActive" && _filterViewModel.IsActive == false)
+                Model.FilterModel = null;
         }
 
-        private void ApplyFilter()
-        {
-            
-        }
         public void ActionsMethod(TranAction action)
         {
             if ((int) action < 11)
             {
                 if (EditActionsExecutorHandler.Do(action, Model))
-                    IsCollectionChanged = true;
+                    Model.IsCollectionChanged = true;
             }
             else
             {
