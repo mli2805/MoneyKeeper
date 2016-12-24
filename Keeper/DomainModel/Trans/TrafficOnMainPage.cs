4a147813e1eb84d0a73aa858ce899bcab89d69d1
mli2805 <mli2805@localhost>
2016-12-24 13:25:23 +0300

    fixed several division by zero bug

--- Keeper/DomainModel/Trans/TrafficOnMainPage.cs
+++ Keeper/DomainModel/Trans/TrafficOnMainPage.cs
@@ -9,7 +9,13 @@ namespace Keeper.DomainModel.Trans
         public decimal Amount;
         public CurrencyCodes Currency;
         public double Rate;
-        public decimal AmountInUsd => Currency == CurrencyCodes.USD ? Amount : Amount/(decimal) Rate;
+        public decimal AmountInUsd => Currency == CurrencyCodes.USD ? 
+            Amount : 
+            Rate.Equals(0) ? 
+                -1 : 
+                Amount/(decimal) Rate;
+
+        private string AmountInUsdString => AmountInUsd == -1 ? "не задан курс" : $"{AmountInUsd:#,0.00}$";
         public string Comment;
 
         public override string ToString()
@@ -19,9 +25,9 @@ namespace Keeper.DomainModel.Trans
                 case CurrencyCodes.USD:
                     return $"{Timestamp:d} {Amount} {Currency.ToString().ToLower()} {Comment}";
                 case CurrencyCodes.BYR:
-                    return $"{Timestamp:d} {Amount:#,0} {Currency.ToString().ToLower()} ({AmountInUsd:#,0.00}$) {Comment}";
+                    return $"{Timestamp:d} {Amount:#,0} {Currency.ToString().ToLower()} ({AmountInUsdString}) {Comment}";
                 default:
-                    return $"{Timestamp:d} {Amount:#,0.00} {Currency.ToString().ToLower()} ({AmountInUsd:#,0.00}$) {Comment}";
+                    return $"{Timestamp:d} {Amount:#,0.00} {Currency.ToString().ToLower()} ({AmountInUsdString}) {Comment}";
             }
 
         }
