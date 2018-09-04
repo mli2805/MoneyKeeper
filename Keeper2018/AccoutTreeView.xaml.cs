a05ebc454ee610d07c160ff28c0358ef71c6c8c7
mli2805 <mli2805@gmail.com>
2018-09-04 22:27:38 +0300

    AccountTreeView

---
+++ Keeper2018/AccoutTreeView.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Keeper2018
+{
+    /// <summary>
+    /// Interaction logic for AccoutTreeView.xaml
+    /// </summary>
+    public partial class AccoutTreeView
+    {
+        Point _lastMouseDown;
+        TreeViewItem _draggedItem, _target;
+        public AccoutTreeView()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    Point currentPosition = e.GetPosition(AccountTree);
+
+                    if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
+                        (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
+                    {
+                        _draggedItem = (TreeViewItem)AccountTree.SelectedItem;
+                        if (_draggedItem != null)
+                        {
+                            DragDropEffects finalDropEffect = DragDrop.DoDragDrop(AccountTree, AccountTree.SelectedValue,
+                                DragDropEffects.Move);
+
+                            //Checking target is not null and item is dragging(moving)
+                            if ((finalDropEffect == DragDropEffects.Move) && (_target != null))
+                            {
+                                // A Move drop was accepted
+                                if (!_draggedItem.Header.ToString().Equals(_target.Header.ToString()))
+                                {
+                                    CopyItem(_draggedItem, _target);
+                                    _target = null;
+                                    _draggedItem = null;
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void treeView_DragOver(object sender, DragEventArgs e)
+        {
+            try
+            {
+
+                Point currentPosition = e.GetPosition(AccountTree);
+
+
+                if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
+                    (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
+                {
+                    // Verify that this is a valid drop and then store the drop target
+                    TreeViewItem item = GetNearestContainer(e.OriginalSource as UIElement);
+                    if (CheckDropTarget(_draggedItem, item))
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.None;
+                    }
+                }
+                e.Handled = true;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void treeView_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+
+                // Verify that this is a valid drop and then store the drop target
+                TreeViewItem TargetItem = GetNearestContainer(e.OriginalSource as UIElement);
+                if (TargetItem != null && _draggedItem != null)
+                {
+                    _target = TargetItem;
+                    e.Effects = DragDropEffects.Move;
+
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+
+
+        }
+        private bool CheckDropTarget(TreeViewItem _sourceItem, TreeViewItem _targetItem)
+        {
+            //Check whether the target item is meeting your condition
+            bool _isEqual = false;
+            if (!_sourceItem.Header.ToString().Equals(_targetItem.Header.ToString()))
+            {
+                _isEqual = true;
+            }
+            return _isEqual;
+
+        }
+        private void CopyItem(TreeViewItem _sourceItem, TreeViewItem _targetItem)
+        {
+
+            //Asking user wether he want to drop the dragged TreeViewItem here or not
+            if (MessageBox.Show("Would you like to drop " + _sourceItem.Header.ToString() + " into " + _targetItem.Header.ToString() + "", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    //adding dragged TreeViewItem in target TreeViewItem
+                    addChild(_sourceItem, _targetItem);
+
+                    //finding Parent TreeViewItem of dragged TreeViewItem 
+                    TreeViewItem ParentItem = FindVisualParent<TreeViewItem>(_sourceItem);
+                    // if parent is null then remove from TreeView else remove from Parent TreeViewItem
+                    if (ParentItem == null)
+                    {
+                        AccountTree.Items.Remove(_sourceItem);
+                    }
+                    else
+                    {
+                        ParentItem.Items.Remove(_sourceItem);
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+        }
+        public void addChild(TreeViewItem _sourceItem, TreeViewItem _targetItem)
+        {
+            // add item in target TreeViewItem 
+            TreeViewItem item1 = new TreeViewItem();
+            item1.Header = _sourceItem.Header;
+            _targetItem.Items.Add(item1);
+            foreach (TreeViewItem item in _sourceItem.Items)
+            {
+                addChild(item, item1);
+            }
+        }
+        static TObject FindVisualParent<TObject>(UIElement child) where TObject : UIElement
+        {
+            if (child == null)
+            {
+                return null;
+            }
+
+            UIElement parent = VisualTreeHelper.GetParent(child) as UIElement;
+
+            while (parent != null)
+            {
+                TObject found = parent as TObject;
+                if (found != null)
+                {
+                    return found;
+                }
+                else
+                {
+                    parent = VisualTreeHelper.GetParent(parent) as UIElement;
+                }
+            }
+
+            return null;
+        }
+        private TreeViewItem GetNearestContainer(UIElement element)
+        {
+            // Walk up the element tree to the nearest tree view item.
+            TreeViewItem container = element as TreeViewItem;
+            while ((container == null) && (element != null))
+            {
+                element = VisualTreeHelper.GetParent(element) as UIElement;
+                container = element as TreeViewItem;
+            }
+            return container;
+        }
+
+    }
+}
--- Keeper2018/ShellViewModel.cs
+++ Keeper2018/ShellViewModel.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
 using Caliburn.Micro;
 
 namespace Keeper2018
 {
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Account Parent { get; set; }
+        public List<Account> Children { get; set; }
+        public int DepositCode { get; set; } = -1;
+    }
+
+
     public class ShellViewModel : Screen, IShell
     {
+        public AccountTreeViewModel AccountTreeViewModel { get; set; } = new AccountTreeViewModel();
+        protected override void OnViewLoaded(object view)
+        {
+            DisplayName = "Keeper 2018";
+        }
 
+      
     }
 }
