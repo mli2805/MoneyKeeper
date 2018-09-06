168a5d7543025810d5c0a1553c73ae48411aa7ba
mli2805 <mli2805@gmail.com>
2018-09-06 08:58:13 +0300

    + Autofac

--- Keeper2018/AppBootstrapper.cs
+++ Keeper2018/AppBootstrapper.cs
@@ -1,36 +1,54 @@
-namespace Keeper2018 {
+namespace Keeper2018 
+{
     using System;
     using System.Collections.Generic;
+    using Autofac;
     using Caliburn.Micro;
 
-    public class AppBootstrapper : BootstrapperBase {
-        SimpleContainer container;
+    public class AppBootstrapper : BootstrapperBase 
+    {
+//        SimpleContainer container;
+        private ILifetimeScope _container;
 
-        public AppBootstrapper() {
+        public AppBootstrapper() 
+        {
             Initialize();
         }
 
-        protected override void Configure() {
-            container = new SimpleContainer();
+//        protected override void Configure() 
+//        {
+//            container = new SimpleContainer();
+//
+//            container.Singleton<IWindowManager, WindowManager>();
+//            container.Singleton<IEventAggregator, EventAggregator>();
+//            container.PerRequest<IShell, ShellViewModel>();
+//        }
 
-            container.Singleton<IWindowManager, WindowManager>();
-            container.Singleton<IEventAggregator, EventAggregator>();
-            container.PerRequest<IShell, ShellViewModel>();
+        protected override object GetInstance(Type service, string key) 
+        {
+          //  return container.GetInstance(service, key);
+            return string.IsNullOrWhiteSpace(key) ?
+                _container.Resolve(service) :
+                _container.ResolveNamed(key, service);    }
+
+        protected override IEnumerable<object> GetAllInstances(Type service) 
+        {
+           // return container.GetAllInstances(service);
+            return _container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
         }
 
-        protected override object GetInstance(Type service, string key) {
-            return container.GetInstance(service, key);
+        protected override void BuildUp(object instance)
+        {
+          //  container.BuildUp(instance);
+            _container.InjectProperties(instance);
         }
 
-        protected override IEnumerable<object> GetAllInstances(Type service) {
-            return container.GetAllInstances(service);
-        }
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<AutofacKeeper>();
+            _container = builder.Build();
 
-        protected override void BuildUp(object instance) {
-            container.BuildUp(instance);
-        }
-
-        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e) {
             DisplayRootViewFor<IShell>();
         }
     }
---
+++ Keeper2018/AutofacKeeper.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Caliburn.Micro;
+
+namespace Keeper2018
+{
+    public sealed class AutofacKeeper : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ShellViewModel>().As<IShell>();
+            builder.RegisterType<WindowManager>().As<IWindowManager>().SingleInstance();
+        }
+    }
+}
