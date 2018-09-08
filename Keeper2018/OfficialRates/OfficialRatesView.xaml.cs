dd0ac832b71b055ae8354630b3e43c9c35332218
mli2805 <mli2805@gmail.com>
2018-09-08 22:05:55 +0300

    + OfficialRatesView

---
+++ Keeper2018/OfficialRates/OfficialRatesView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Keeper2018
+{
+    /// <summary>
+    /// Interaction logic for OfficialRatesView.xaml
+    /// </summary>
+    public partial class OfficialRatesView
+    {
+        public OfficialRatesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Keeper2018/OfficialRates/OfficialRatesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Caliburn.Micro;
+
+namespace Keeper2018
+{
+    public class OfficialRatesViewModel : Screen
+    {
+        protected override void OnViewLoaded(object view)
+        {
+            DisplayName = "NB RB. Official rates.";
+        }
+    }
+}
