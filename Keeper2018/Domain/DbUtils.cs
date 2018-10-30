aa512e7a70b66238098d64fbfc70d2be9b37e8b9
mli2805 <mli2805@gmail.com>
2018-10-30 10:05:46 +0300

    just reordering

--- Keeper2018/Domain/DbUtils.cs
+++ Keeper2018/Domain/DbUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using Keeper2018.Properties;
 
@@ -7,6 +8,12 @@ namespace Keeper2018
 {
     public static class DbUtils
     {
+        public class Db2018
+        {
+            public ObservableCollection<Account> Accounts { get; set; }
+            public ObservableCollection<NbRbRate> OfficialRates { get; set; }
+        }
+
         public static Account GetById(int id, ICollection<Account> roots)
         {
             foreach (var account in roots)
