b2058f4443099be1bbf4f1d2470f7c8ac39b59f8
mli2805 <mli2805@gmail.com>
2018-10-30 10:36:39 +0300

    Revert "removed intermediate class"
    This reverts commit 43ecef30fc514837aca98115157d9a732005f494.

--- Keeper2018/Domain/DB/NbRbRate.cs
+++ Keeper2018/Domain/DB/NbRbRate.cs
@@ -5,8 +5,7 @@ namespace Keeper2018
     public class NbRbRate
     {
         public DateTime Date { get; set; }
-        public Rate Usd { get; set; } = new Rate();
-        public Rate Euro { get; set; } = new Rate();
-        public Rate Rur { get; set; } = new Rate();
+        public MainCurrenciesRates Values { get; set; } = new MainCurrenciesRates();
+
     }
 }
