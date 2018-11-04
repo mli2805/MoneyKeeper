2b13ff431b06895c06514285e0a3115b89e55df1
mli2805 <mli2805@gmail.com>
2018-11-04 21:17:39 +0300

    transactions button

--- Keeper2018/Transactions/TransactionsViewModel.cs
+++ Keeper2018/Transactions/TransactionsViewModel.cs
@@ -1,10 +1,13 @@
-﻿using Caliburn.Micro;
+﻿using System.Collections.ObjectModel;
+using Caliburn.Micro;
 
 namespace Keeper2018
 {
     public class TransactionsViewModel : Screen
     {
         private readonly KeeperDb _keeperDb;
+
+        public ObservableCollection<Transaction> Rows { get; set; }
 
         public TransactionsViewModel(KeeperDb keeperDb)
         {
