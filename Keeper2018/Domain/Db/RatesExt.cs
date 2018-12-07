a9517ecc9d9df665705b387f24df2c8782da7367
mli2805 <mli2805@gmail.com>
2018-12-07 09:00:00 +0300

    main icon, missing rate

--- Keeper2018/Domain/Db/RatesExt.cs
+++ Keeper2018/Domain/Db/RatesExt.cs
@@ -11,36 +11,34 @@ namespace Keeper2018
             if (officialRates == null) return null;
             switch (currency)
             {
-                    case CurrencyCode.BYN : return officialRates.NbRates.Usd;
-                    case CurrencyCode.BYR : return officialRates.NbRates.Usd;
-                    case CurrencyCode.EUR : return officialRates.NbRates.Euro;
-                    case CurrencyCode.RUB : return officialRates.NbRates.Rur;
+                case CurrencyCode.BYN: return officialRates.NbRates.Usd;
+                case CurrencyCode.BYR: return officialRates.NbRates.Usd;
+                case CurrencyCode.EUR: return officialRates.NbRates.Euro;
+                case CurrencyCode.RUB: return officialRates.NbRates.Rur;
             }
-
             return null;
         }
 
         public static decimal AmountInUsd(this KeeperDb db, DateTime date, CurrencyCode? currency, decimal amount)
         {
             if (currency == CurrencyCode.USD) return amount;
-            var rateLine = db.Bin.OfficialRates.Last(r => r.Date.Date <= date);
+            var rateLine =  currency == CurrencyCode.BYN 
+                ? db.Bin.OfficialRates.Last(r => r.Date.Date <= date && Math.Abs(r.MyUsdRate.Value) > 0.1) 
+                : db.Bin.OfficialRates.Last(r => r.Date.Date <= date);
             return currency == CurrencyCode.BYR || currency == CurrencyCode.BYN
                 ? amount / (decimal)rateLine.MyUsdRate.Value
                 : currency == CurrencyCode.EUR
                     ? amount * (decimal)rateLine.NbRates.Euro.Value / (decimal)rateLine.NbRates.Usd.Value
                     : amount * (decimal)rateLine.NbRates.Rur.Value / rateLine.NbRates.Rur.Unit / (decimal)rateLine.NbRates.Usd.Value;
-
         }
 
         public static string AmountInUsdString(this KeeperDb db, DateTime date, CurrencyCode? currency, decimal amount)
         {
             var shortLine = $"{amount} {currency.ToString().ToLower()}";
-            if (currency != CurrencyCode.USD)
-            {
-                var amountInUsd = db.AmountInUsd(date, currency, amount);
-                shortLine = shortLine + $" ({amountInUsd:#.00}$)";
-            }
-            return shortLine;
+            if (currency == CurrencyCode.USD) return shortLine;
+
+            var amountInUsd = db.AmountInUsd(date, currency, amount);
+            return shortLine + $" ({amountInUsd:#.00}$)";
         }
 
         public static decimal BalanceInUsd(this KeeperDb db, DateTime date, Balance balance)
@@ -48,10 +46,9 @@ namespace Keeper2018
             decimal amountInUsd = 0;
             foreach (var pair in balance.Currencies)
             {
-                amountInUsd = amountInUsd +
-                              (pair.Key == CurrencyCode.USD
-                                  ? pair.Value
-                                  : db.AmountInUsd(date, pair.Key, pair.Value));
+                amountInUsd = amountInUsd + (pair.Key == CurrencyCode.USD
+                                                  ? pair.Value
+                                                  : db.AmountInUsd(date, pair.Key, pair.Value));
             }
             return amountInUsd;
         }
