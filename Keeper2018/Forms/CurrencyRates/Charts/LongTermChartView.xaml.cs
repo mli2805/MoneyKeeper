50ea10bce6fe7c69e7b2a1f86704102122264dff
mli2805 <mli2805@gmail.com>
2018-12-19 11:16:58 +0300

    folder renamed

---
+++ Keeper2018/Forms/CurrencyRates/Charts/LongTermChartView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Keeper2018
+{
+    /// <summary>
+    /// Interaction logic for LongTermChartView.xaml
+    /// </summary>
+    public partial class LongTermChartView
+    {
+        public LongTermChartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
