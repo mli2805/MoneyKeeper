3c3b4ea432e837ca8f79dd5117b71ff4b8668f84
mli2805 <mli2805@gmail.com>
2019-02-05 20:50:43 +0300

    last day included at last

---
+++ Keeper2018/Forms/Charts/BalanceAndSaldoModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace Keeper2018
+{
+    public class BalanceAndSaldoModel
+    {
+        private KeeperDb _db;
+        public LineSeries DailyBalancesSeries { get; set; }
+        public ColumnSeries MonthlySaldoSeries { get; set; }
+        public ColumnSeries AnnualSaldoSeries { get; set; }
+
+        public List<string> MonthLabels = new List<string>();
+        public List<string> YearLabels = new List<string>();
+
+        public void Initialize(KeeperDb db)
+        {
+            _db = db;
+            DailyBalancesSeries = new LineSeries()
+            { Title = "Дневные остатки", Color = OxyColors.BlueViolet };
+            MonthlySaldoSeries = new ColumnSeries()
+            { Title = "Сальдо по месяцам", FillColor = OxyColors.Blue, NegativeFillColor = OxyColors.Red };
+            AnnualSaldoSeries = new ColumnSeries()
+            { Title = "Сальдо по годам", FillColor = OxyColors.Blue, NegativeFillColor = OxyColors.Red };
+        }
+
+        private Balance _balance = new Balance();
+        private int _monthIndex;
+        private int _yearIndex;
+        private double _previousMonth;
+        private double _previousYear;
+        private double _balanceInUsd;
+        private DateTime _currentDate = new DateTime(2001, 12, 31);
+        private DateTime _previousDate;
+        public void PrepareSeries()
+        {
+            foreach (var tran in _db.Bin.Transactions.Values)
+            {
+                if (!tran.Timestamp.Date.Equals(_currentDate))
+                {
+                    RegisterDay(tran);
+                }
+                RegisterTran(tran);
+            }
+            RegisterDay(null);
+        }
+
+        private void RegisterDay(Transaction tran)
+        {
+            _balanceInUsd = (double) _db.BalanceInUsd(_currentDate, _balance);
+            DailyBalancesSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(_currentDate), _balanceInUsd));
+
+            _previousDate = _currentDate;
+            _currentDate = tran?.Timestamp.Date ?? _currentDate.AddMonths(13);
+
+            if (_currentDate.Month != _previousDate.Month)
+            {
+                if (_previousDate.Year != 2001)
+                {
+                    MonthLabels.Add(_previousDate.ToString("MM/yyyy"));
+                    MonthlySaldoSeries.Items.Add(new ColumnItem(_balanceInUsd - _previousMonth, _monthIndex));
+                    _monthIndex++;
+                }
+
+                _previousMonth = _balanceInUsd;
+            }
+
+            if (_currentDate.Year != _previousDate.Year)
+            {
+                if (_previousDate.Year != 2001)
+                {
+                    YearLabels.Add(_previousDate.ToString("yyyy"));
+                    AnnualSaldoSeries.Items.Add(new ColumnItem(_balanceInUsd - _previousYear, _yearIndex));
+                    _yearIndex++;
+                }
+
+                _previousYear = _balanceInUsd;
+            }
+        }
+
+        private void RegisterTran(Transaction tran)
+        {
+            switch (tran.Operation)
+            {
+                case OperationType.Доход:
+                    _balance.Add(tran.Currency, tran.Amount);
+                    break;
+                case OperationType.Расход:
+                    _balance.Sub(tran.Currency, tran.Amount);
+                    break;
+                case OperationType.Перенос:
+                    if (tran.Timestamp.Date.Year == 2001)
+                        _balance.Add(tran.Currency, tran.Amount);
+                    break;
+                case OperationType.Обмен:
+                    _balance.Sub(tran.Currency, tran.Amount);
+                    // ReSharper disable once PossibleInvalidOperationException
+                    _balance.Add((CurrencyCode)tran.CurrencyInReturn, tran.AmountInReturn);
+                    break;
+            }
+        }
+    }
+}
--- Keeper2018/Forms/Charts/BalancesAndSaldosViewModel.cs
+++ Keeper2018/Forms/Charts/BalancesAndSaldosViewModel.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using Caliburn.Micro;
 using OxyPlot;
 using OxyPlot.Axes;
-using OxyPlot.Series;
+
 #pragma warning disable 612
 
 namespace Keeper2018
 {
     public class BalancesAndSaldosViewModel : Screen
     {
-        private KeeperDb _db;
         public PlotModel DailyBalancesModel { get; set; }
-        public LineSeries DailyBalancesSeries { get; set; }
 
+        private Visibility _dailyBalancesModelVisibility = Visibility.Visible;
         public Visibility DailyBalancesModelVisibility
         {
-            get { return _dailyBalancesModelVisibility; }
+            get => _dailyBalancesModelVisibility;
             set
             {
                 if (value == _dailyBalancesModelVisibility) return;
@@ -27,11 +24,11 @@ namespace Keeper2018
         }
 
         public PlotModel MonthlySaldoModel { get; set; }
-        public ColumnSeries MonthlySaldoSeries { get; set; }
 
+        private Visibility _monthlySaldoModelVisibility = Visibility.Collapsed;
         public Visibility MonthlySaldoModelVisibility
         {
-            get { return _monthlySaldoModelVisibility; }
+            get => _monthlySaldoModelVisibility;
             set
             {
                 if (value == _monthlySaldoModelVisibility) return;
@@ -41,11 +38,11 @@ namespace Keeper2018
         }
 
         public PlotModel AnnualSaldoModel { get; set; }
-        public ColumnSeries AnnualSaldoSeries { get; set; }
 
+        private Visibility _annualSaldoModelVisibility = Visibility.Collapsed;
         public Visibility AnnualSaldoModelVisibility
         {
-            get { return _annualSaldoModelVisibility; }
+            get => _annualSaldoModelVisibility;
             set
             {
                 if (value == _annualSaldoModelVisibility) return;
@@ -56,22 +53,17 @@ namespace Keeper2018
 
         public void Initialize(KeeperDb db)
         {
-            _db = db;
+            var model = new BalanceAndSaldoModel();
+            model.Initialize(db);
+            model.PrepareSeries();
 
-            InitializeDailyBalances();
-            InitializeMonthlySaldo();
+            InitializeDailyBalances(model);
+            InitializeMonthlyAndAnnualSaldo(model);
+         }
 
-            PrepareSeries();
-
-            DailyBalancesModel.Series.Add(DailyBalancesSeries);
-            MonthlySaldoModel.Series.Add(MonthlySaldoSeries);
-            AnnualSaldoModel.Series.Add(AnnualSaldoSeries);
-        }
-
-        private void InitializeDailyBalances()
+        private void InitializeDailyBalances(BalanceAndSaldoModel model)
         {
             DailyBalancesModel = new PlotModel();
-            DailyBalancesSeries = new LineSeries() { Title = "Дневные остатки", Color = OxyColors.BlueViolet };
 
             DailyBalancesModel.Axes.Add(new DateTimeAxis()
             {
@@ -88,99 +80,22 @@ namespace Keeper2018
                 MajorGridlineStyle = LineStyle.Automatic,
                 MinorGridlineStyle = LineStyle.Automatic,
             });
+
+            DailyBalancesModel.Series.Add(model.DailyBalancesSeries);
         }
 
-        private void InitializeMonthlySaldo()
+        private void InitializeMonthlyAndAnnualSaldo(BalanceAndSaldoModel model)
         {
             MonthlySaldoModel = new PlotModel();
-            MonthlySaldoSeries = new ColumnSeries() { Title = "Сальдо по месяцам", FillColor = OxyColors.Blue, NegativeFillColor = OxyColors.Red };
             AnnualSaldoModel = new PlotModel();
-            AnnualSaldoSeries = new ColumnSeries()
-            { Title = "Сальдо по годам", FillColor = OxyColors.Blue, NegativeFillColor = OxyColors.Red };
             MonthlySaldoModel.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Dash });
             AnnualSaldoModel.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Dash });
-        }
 
-        private Balance _balance = new Balance();
-        private Visibility _dailyBalancesModelVisibility = Visibility.Visible;
-        private Visibility _monthlySaldoModelVisibility = Visibility.Collapsed;
-        private Visibility _annualSaldoModelVisibility = Visibility.Collapsed;
+            MonthlySaldoModel.Series.Add(model.MonthlySaldoSeries);
+            AnnualSaldoModel.Series.Add(model.AnnualSaldoSeries);
 
-        private void PrepareSeries()
-        {
-            var monthLabels = new List<string>();
-            var yearLabels = new List<string>();
-            int monthIndex = 0;
-            int yearIndex = 0; 
-            double previousMonth = 0.0;
-            double previousYear = 0.0;
-            double balanceInUsd = 0.0, value;
-            var currentDate = new DateTime(2001, 12, 31);
-            foreach (var tran in _db.Bin.Transactions.Values)
-            {
-                if (!tran.Timestamp.Date.Equals(currentDate))
-                {
-                    var day = DateTimeAxis.ToDouble(currentDate);
-                    balanceInUsd = (double)_db.BalanceInUsd(currentDate, _balance);
-                    DailyBalancesSeries.Points.Add(new DataPoint(day, balanceInUsd));
-
-                    var previousDate = currentDate;
-                    currentDate = tran.Timestamp.Date;
-
-                    if (currentDate.Month != previousDate.Month)
-                    {
-                        if (previousDate.Year != 2001)
-                        {
-                            monthLabels.Add(previousDate.ToString("MM/yyyy"));
-                            value = balanceInUsd - previousMonth;
-                            MonthlySaldoSeries.Items.Add(new ColumnItem(value, monthIndex));
-                            monthIndex++;
-                        }
-                        previousMonth = balanceInUsd;
-                    }
-                    if (currentDate.Year != previousDate.Year)
-                    {
-                        if (previousDate.Year != 2001)
-                        {
-                            yearLabels.Add(previousDate.ToString("yyyy"));
-                            value = balanceInUsd - previousYear;
-                            AnnualSaldoSeries.Items.Add(new ColumnItem(value, yearIndex));
-                            yearIndex++;
-                        }
-                        previousYear = balanceInUsd;
-                    }
-                }
-                RegisterTran(tran);
-            }
-            monthLabels.Add(currentDate.ToString("MM/yyyy"));
-            MonthlySaldoSeries.Items.Add(new ColumnItem(balanceInUsd - previousMonth, monthIndex));
-            yearLabels.Add(currentDate.ToString("yyyy"));
-            AnnualSaldoSeries.Items.Add(new ColumnItem(balanceInUsd - previousYear, yearIndex));
-                               
-            MonthlySaldoModel.Axes.Add(new CategoryAxis(null, monthLabels.ToArray()));
-            AnnualSaldoModel.Axes.Add(new CategoryAxis(null, yearLabels.ToArray()));
-        }
-
-        private void RegisterTran(Transaction tran)
-        {
-            switch (tran.Operation)
-            {
-                case OperationType.Доход:
-                    _balance.Add(tran.Currency, tran.Amount);
-                    break;
-                case OperationType.Расход:
-                    _balance.Sub(tran.Currency, tran.Amount);
-                    break;
-                case OperationType.Перенос:
-                    if (tran.Timestamp.Date.Year == 2001)
-                        _balance.Add(tran.Currency, tran.Amount);
-                    break;
-                case OperationType.Обмен:
-                    _balance.Sub(tran.Currency, tran.Amount);
-                    // ReSharper disable once PossibleInvalidOperationException
-                    _balance.Add((CurrencyCode)tran.CurrencyInReturn, tran.AmountInReturn);
-                    break;
-            }
+            MonthlySaldoModel.Axes.Add(new CategoryAxis(null, model.MonthLabels.ToArray()));
+            AnnualSaldoModel.Axes.Add(new CategoryAxis(null, model.YearLabels.ToArray()));
         }
 
         private int _model = 1;
