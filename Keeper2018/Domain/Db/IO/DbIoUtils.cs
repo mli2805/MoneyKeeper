81ce586b69d910552a6e4ba29a8050faa6531d47
mli2805 <mli2805@gmail.com>
2020-02-20 11:55:27 +0300

    google drive instead of dropbox

--- Keeper2018/Domain/Db/IO/DbIoUtils.cs
+++ Keeper2018/Domain/Db/IO/DbIoUtils.cs
@@ -1,6 +1,4 @@
-using System;
 using System.IO;
-using System.Linq;
 
 namespace Keeper2018
 {
@@ -9,52 +7,36 @@ namespace Keeper2018
         // export from old Keeper
         public static string GetOldTxtFullPath(string filename)
         {
-            var keeperInDropboxFullPath = GetKeeper2018InDropboxPath();
+            var keeperInDropboxFullPath = PathFinder.GetKeeper2018BasePath();
             var dbPath = keeperInDropboxFullPath + @"\OldKeeperTxt";
             return Path.Combine(dbPath, filename);
         }
 
         public static string GetBackupFilePath(string filename)
         {
-            var keeperInDropboxFullPath = GetKeeper2018InDropboxPath();
+            var keeperInDropboxFullPath = PathFinder.GetKeeper2018BasePath();
             var dbPath = keeperInDropboxFullPath + @"\Backup";
             return Path.Combine(dbPath, filename);
         }
 
         public static string GetReportFullPath(string filename)
         {
-            var keeperInDropboxFullPath = GetKeeper2018InDropboxPath();
+            var keeperInDropboxFullPath = PathFinder.GetKeeper2018BasePath();
             var dbPath = keeperInDropboxFullPath + @"\Reports";
             return Path.Combine(dbPath, filename);
         }
 
         public static string GetBackupPath()
         {
-            var keeperInDropboxFullPath = GetKeeper2018InDropboxPath();
+            var keeperInDropboxFullPath = PathFinder.GetKeeper2018BasePath();
             return keeperInDropboxFullPath + @"\Backup";
         }
 
         public static string GetDbFullPath()
         {
-            var keeperInDropboxFullPath = GetKeeper2018InDropboxPath();
+            var keeperInDropboxFullPath = PathFinder.GetKeeper2018BasePath();
             return Path.Combine(keeperInDropboxFullPath, @"Db\KeeperDb.bin");
         }
 
-        private static string GetKeeper2018InDropboxPath()
-        {
-            return Path.Combine(GetCurrentDropboxPath(), @"Keeper2018");
-        }
-
-        private static string GetCurrentDropboxPath()
-        {
-            const string infoPath = @"Dropbox\info.json";
-            var jsonPath = Path.Combine(Environment.GetEnvironmentVariable("LocalAppData") ?? throw new InvalidOperationException(), infoPath);
-            if (!File.Exists(jsonPath)) jsonPath = Path.Combine(Environment.GetEnvironmentVariable("AppData") ?? throw new InvalidOperationException(), infoPath);
-            if (!File.Exists(jsonPath)) throw new Exception("Dropbox could not be found!");
-            var strings = File.ReadAllText(jsonPath).Split('\"');
-            var index = strings.ToList().IndexOf("path");
-            var dropboxPath = strings[index + 2].Replace(@"\\", @"\");
-            return dropboxPath;
-        }
     }
 }
