18726ca98d0504d43a0ac389dc7006b3e6c82d15
mli2805 <mli2805@gmail.com>
2020-02-20 11:04:53 +0300

    Cleanup

--- Keeper2018/Forms/Shell/ShellViewModel.cs
+++ Keeper2018/Forms/Shell/ShellViewModel.cs
@@ -43,35 +43,11 @@ namespace Keeper2018
                 return;
             }
 
-            //            NormalizeTimestamps();
-
             _dbLoader.ExpandBinToDb(_keeperDb);
             var account = _keeperDb.AccountsTree.First(r => r.Name == "Мои");
             account.IsSelected = true;
             ShellPartsBinder.SelectedAccountModel = account;
         }
-
-        //        private void NormalizeTimestamps()
-        //        {
-        //            var previousDate = _keeperDb.Bin.Transactions.Values.First().Timestamp.Date;
-        //            var count = 1;
-        //            foreach (var pair in _keeperDb.Bin.Transactions)
-        //            {
-        //                var tran = pair.Value;
-        //                if (previousDate.Date == tran.Timestamp.Date)
-        //                {
-        //                    tran.Timestamp = previousDate.AddMinutes(count);
-        //                    count++;
-        //                }
-        //                else
-        //                {
-        //                    tran.Timestamp = tran.Timestamp.Date.AddMinutes(1);
-        //                    count = 2;
-        //                }
-        //
-        //                previousDate = tran.Timestamp.Date;
-        //            }
-        //        }
 
         public override async void CanClose(Action<bool> callback)
         {
