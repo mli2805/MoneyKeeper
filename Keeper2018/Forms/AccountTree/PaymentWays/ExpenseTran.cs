1340510693a7aba38a12be2ba50c0a63d211e3fa
mli2805 <mli2805@gmail.com>
2020-11-30 11:14:54 +0300

    display expense transactions grouped by payment way

---
+++ Keeper2018/Forms/AccountTree/PaymentWays/ExpenseTran.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using KeeperDomain;
+
+namespace Keeper2018
+{
+    public class ExpenseTran
+    {
+        public DateTime Timestamp { get; set; }
+        public int Receipt { get; set; }
+        public decimal Amount { get; set; }
+        public CurrencyCode Currency { get; set; }
+        public List<int> Tags { get; set; }
+        public string Comment { get; set; }
+
+
+        public ExpenseTran(Transaction tr)
+        {
+            Timestamp = tr.Timestamp;
+            Receipt = tr.Receipt;
+            Amount = tr.Amount;
+            Currency = tr.Currency;
+            Tags = tr.Tags;
+            Comment = tr.Comment;
+        }
+
+        public string ToString(KeeperDb db)
+        {
+            return Timestamp.ToString("dd/MM/yyyy HH:mm") + " ; " +
+                   Amount.ToString(new CultureInfo("en-US")) + " ; " + Currency + " ; " +
+                   TagsToString(db) + " ; " + Comment;
+        }
+
+        private string TagsToString(KeeperDb db)
+        {
+            if (Tags == null || Tags.Count == 0) return " ";
+
+
+            string result = "";
+            foreach (var t in Tags)
+            {
+                var acc = db.AcMoDict[t];
+                if (Receipt > 0 && acc.Is(179))
+                    return acc.Name;
+                result = result + acc.Name + " | ";
+            }
+            result = result.Substring(0, result.Length - 3);
+            return result;
+        }
+    }
+}
--- Keeper2018/Forms/AccountTree/PaymentWays/PaymentWaysViewModel.cs
+++ Keeper2018/Forms/AccountTree/PaymentWays/PaymentWaysViewModel.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using Caliburn.Micro;
 using KeeperDomain;
+using Microsoft.Win32;
 
 namespace Keeper2018
 {
     public class PaymentWaysViewModel : Screen
     {
         private readonly KeeperDb _db;
+        private AccountModel _cardAccountModel;
+        private Period _period;
         private List<Transaction> _trans;
+        public ObservableCollection<string> Lines { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<string> Totals { get; set; } = new ObservableCollection<string>();
         public decimal Total { get; set; }
 
         public PaymentWaysViewModel(KeeperDb db)
@@ -25,19 +31,73 @@ namespace Keeper2018
 
         public void Initialize(AccountModel accountModel)
         {
-            var period = DateTime.Today.GetFullMonthForDate();
+            _cardAccountModel = accountModel;
+            _period = DateTime.Today.GetFullMonthForDate();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
             _trans = _db.Bin.Transactions.Values.Where(t => t.Operation == OperationType.Расход
-                                                        && t.MyAccount == accountModel.Id
-                                                        && period.Includes(t.Timestamp)).ToList();
-            Total = _trans.Sum(t => t.Amount);
+                                                            && t.MyAccount == _cardAccountModel.Id
+                                                            && _period.Includes(t.Timestamp)).ToList();
+            Lines.Clear();
+            Totals.Clear();
+            foreach (var paymentWay in Enum.GetValues(typeof(PaymentWay)).OfType<PaymentWay>().ToList())
+            {
+                var onePaymentWay = _trans.Where(t => t.PaymentWay == paymentWay).ToList();
+                if (onePaymentWay.Any())
+                {
+                    Lines.Add($"        {paymentWay}:   {onePaymentWay.Sum(t => t.Amount)}");
+                    Totals.Add($"{paymentWay}:   {onePaymentWay.Sum(t => t.Amount)}");
+                    foreach (var receipt in ShrinkReceipts(onePaymentWay))
+                        Lines.Add(receipt);
+                    Lines.Add("");
+                }
+            }
+
+            Totals.Add($"Всего по карте:   {_trans.Sum(t => t.Amount)}");
+        }
+
+        public void StepMonthBefore()
+        {
+            _period = _period.StartDate.AddMonths(-1).GetFullMonthForDate();
+            Initialize();
+        }
+
+        public void StepMonthAfter()
+        {
+            _period = _period.StartDate.AddMonths(1).GetFullMonthForDate();
+            Initialize();
         }
 
         public void Export()
         {
-            var content = _trans.Select(t => t.Dump());
-            File.WriteAllLines(@"c:\temp\joker.csv", content);
+            var keeperInDropboxFullPath = PathFinder.GetKeeper2018BasePath();
+            var dbPath = keeperInDropboxFullPath + @"\Reports";
+
+            var dlg = new SaveFileDialog { InitialDirectory = dbPath, DefaultExt = "csv", Filter = "Csv files|*.csv" };
+            if (dlg.ShowDialog() != true)
+                return;
+            File.WriteAllLines(dlg.FileName, Lines);
         }
         public void Close() { TryClose(); }
 
+        private List<string> ShrinkReceipts(List<Transaction> trans)
+        {
+            var singles = trans.Where(t => t.Receipt == 0).Select(t => new ExpenseTran(t)).ToList();
+            var groups = trans.Where(t => t.Receipt > 0).GroupBy(t => t.Receipt.ToString() + "_" + t.Timestamp.Date).ToList();
+            foreach (var group in groups)
+            {
+                if (group == null) continue; // just because of warning
+                var tr = new ExpenseTran(group.AsEnumerable().First())
+                {
+                    Amount = group.Sum(t => t.Amount),
+                    Comment = $"чек {group.Key.Substring(0, group.Key.IndexOf('_'))}"
+                };
+                singles.Add(tr);
+            }
+            return singles.OrderBy(t => t.Timestamp).Select(t => t.ToString(_db)).ToList();
+        }
     }
 }
