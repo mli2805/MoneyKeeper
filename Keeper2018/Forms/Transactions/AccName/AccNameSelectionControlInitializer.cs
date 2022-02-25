f9d54118bedd112f018c558daae8283a3d3d5995
mli2805 <mli2805@gmail.com>
2022-02-25 11:42:28 +0300

    acc name selector fixed

--- Keeper2018/Forms/Transactions/AccName/AccNameSelectionControlInitializer.cs
+++ Keeper2018/Forms/Transactions/AccName/AccNameSelectionControlInitializer.cs
@@ -54,17 +54,17 @@ namespace Keeper2018
             {
                 case OperationType.Доход:
                     return Build("Кто, за что", ButtonCollections.ForIncomeTags,
-                        _comboTreesProvider.AccNamesForIncomeTags, tran.Tags?.First()?.Id ?? 443);
+                        _comboTreesProvider.AccNamesForIncomeTags, tran.Tags?.FirstOrDefault()?.Id ?? 443);
                 case OperationType.Расход:
                     return Build("Кому, за что", ButtonCollections.ForExpenseTags,
-                        _comboTreesProvider.AccNamesForExpenseTags, tran.Tags?.First()?.Id ?? 256);
+                        _comboTreesProvider.AccNamesForExpenseTags, tran.Tags?.FirstOrDefault()?.Id ?? 256);
                 case OperationType.Перенос:
                     return Build("Теги", ButtonCollections.ForTransferTags,
-                        _comboTreesProvider.AccNamesForTransferTags, tran.Tags?.First()?.Id ?? 579);
+                        _comboTreesProvider.AccNamesForTransferTags, tran.Tags?.FirstOrDefault()?.Id ?? 579);
                 // case OperationType.Обмен:
                 default:
                     return Build("Теги", ButtonCollections.ForExchangeTags,
-                        _comboTreesProvider.AccNamesForExchangeTags, tran.Tags?.First()?.Id ?? 339);
+                        _comboTreesProvider.AccNamesForExchangeTags, tran.Tags?.FirstOrDefault()?.Id ?? 339);
             }
         }
 
