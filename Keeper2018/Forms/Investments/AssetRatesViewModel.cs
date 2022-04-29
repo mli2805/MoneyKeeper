83968fc81267e470c0a4bacdfa87178cf5698ee6
mli2805 <mli2805@gmail.com>
2022-04-29 11:33:31 +0300

    assets rates input

--- Keeper2018/Forms/Investments/AssetRatesViewModel.cs
+++ Keeper2018/Forms/Investments/AssetRatesViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Controls;
 using Caliburn.Micro;
 using KeeperDomain;
 
@@ -14,6 +15,8 @@ namespace Keeper2018
         public ObservableCollection<AssetRate> Rates { get; set; }
         public AssetRate SelectedRate { get; set; }
         public List<InvestmentAsset> Assets { get; set; }
+
+        public DateTime SelectedDate { get; set; } = DateTime.Today;
 
         public AssetRatesViewModel(KeeperDataModel dataModel)
         {
@@ -36,6 +39,24 @@ namespace Keeper2018
         {
             if (SelectedRate != null)
                 Rates.Remove(SelectedRate);
+        }
+
+        public void AddForDate()
+        {
+            var lastId = Rates.Max(r => r.Id);
+            foreach (var asset in Assets.Where(a=>a.Ticker != "CASH"))
+            {
+                var prev = Rates.FirstOrDefault(r => r.TickerId == asset.Id);
+                Rates.Add(new AssetRate()
+                {
+                    Id = ++lastId, 
+                    TickerId = asset.Id, 
+                    Currency = prev?.Currency ?? CurrencyCode.RUB,
+                    Unit = prev?.Unit ?? 1,
+                    Value = 0,
+                    Date = SelectedDate,
+                });
+            }
         }
 
         public override void CanClose(Action<bool> callback)
