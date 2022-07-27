11f0cccc2ac7de196d3cfe46f8a8a5a285cf6ccb
mli2805 <mli2805@gmail.com>
2022-07-27 16:45:59 +0300

    commit to have clean tree

--- Keeper2018/Forms/Investments/AnalysisTools/AssetOnPeriodReportComposer.cs
+++ Keeper2018/Forms/Investments/AnalysisTools/AssetOnPeriodReportComposer.cs
@@ -6,6 +6,17 @@ namespace Keeper2018
 {
     public static class AssetOnPeriodReportComposer
     {
+        public static AssetOnPeriodReportModel CreateReport(this AssetOnPeriodData assetOnPeriodData)
+        {
+            return new AssetOnPeriodReportModel()
+            {
+                BeforeState = assetOnPeriodData.Before.CreateBeforeColumn().ToList(),
+                OnStartState = assetOnPeriodData.CreateOnStartColumn().ToList(),
+                InBetweenTrans = assetOnPeriodData.CreateInBetweenColumn().ToList(),
+                AtEndState = assetOnPeriodData.CreateAtEndColumn().ToList(),
+            };
+        }
+
         private static List<string> CreateBeforeColumn(this AssetState assetState)
         {
             List<string> result = new List<string>();
@@ -75,7 +86,7 @@ namespace Keeper2018
             result.Add(assetState.Caption);
             if (assetState.Asset.TrustAccount.Currency == CurrencyCode.RUB)
             {
-                var currencyLine = ChangesOverPeriod(
+                var currencyLine = GetChangesLineForTwoFigures(
                     assetOnPeriodData.OnStart.CurrentCurrencyRate, assetState.CurrentCurrencyRate, false);
                 result.Add($"   курс рубля {currencyLine} ");
             }
@@ -102,151 +113,132 @@ namespace Keeper2018
                 result.Add(currentState.CurrentCurrencyRateStr);
             result.Add("");
 
-            result.Add($"Количество: {currentState.Quantity} шт.");
+            result.AddRange(currentState.FillState());
+            result.AddRange(currentState.Quantity > 0
+                ? CreateAtEndColumnForExistingAsset(assetOnPeriodData)
+                : CreateAtEndColumnForAssetSoldBefore(assetOnPeriodData));
 
-            result.Add(currentState.Asset.AssetType == AssetType.Bond
-                ? $" цена + НКД: ({currentState.CurrentAssetRate:N} + {currentState.AccumulatedCouponIncome:F}) = {currentState.AveragePriceStr}"
-                : $" цена: {currentState.AveragePriceStr}");
-            result.Add($"Стоимость: {currentState.PriceStr}");
-            result.Add("");
-
-            if (assetOnPeriodData.OnStart.Price > 0)
-            {
-                result.Add($"За период {assetOnPeriodData.InBetween.Caption}");
-                result.AddRange(FillPeriodChanges(assetOnPeriodData.OnStart, currentState));
-                var profitOverPeriod = currentState.PriceInUsd - assetOnPeriodData.OnStart.PriceInUsd
-                                                               - assetOnPeriodData.InBetween.OperationFeesInUsd + assetOnPeriodData.InBetween.ReceivedCouponInUsd;
-                var resultStr = $"Результат в usd: {currentState.PriceInUsd:F} - {assetOnPeriodData.OnStart.PriceInUsd:F}";
-                if (assetOnPeriodData.InBetween.OperationFeesInUsd > 0)
-                    resultStr += $" - {assetOnPeriodData.InBetween.OperationFeesInUsd:F}";
-                if (assetOnPeriodData.InBetween.ReceivedCouponInUsd > 0)
-                    resultStr += $" + {assetOnPeriodData.InBetween.ReceivedCouponInUsd:F}";
-                result.Add(resultStr);
-                result.Add($"                        = {profitOverPeriod:N} usd  ({profitOverPeriod / assetOnPeriodData.OnStart.PriceInUsd * 100:F1}%)");
-                result.Add("");
-
-                result.Add("С момента приобретения");
-                result.AddRange(FillAllChanges(assetOnPeriodData.Before, currentState));
-                var fullPriceBefore = assetOnPeriodData.Before.PriceInUsd + assetOnPeriodData.Before.OperationFeesInUsd;
-                var profitTotal = currentState.PriceInUsd + currentState.ReceivedCouponInUsd - fullPriceBefore;
-                var totalStr = $"Результат в usd: {currentState.PriceInUsd:F} - {assetOnPeriodData.Before.PriceInUsd:F} - {currentState.OperationFeesInUsd:F}";
-                if (currentState.ReceivedCouponInUsd > 0)
-                    totalStr += $" + {currentState.ReceivedCouponInUsd:F}";
-                result.Add(totalStr);
-                result.Add($"                        = {profitTotal:N} usd ({profitTotal / assetOnPeriodData.Before.PriceInUsd * 100:F1}%)");
-                result.Add("");
-            }
 
             return result;
         }
 
-        private static List<string> FillAllChanges(AssetState fromState, AssetState currentState)
+        private static List<string> CreateAtEndColumnForAssetSoldBefore(this AssetOnPeriodData assetOnPeriodData)
         {
             List<string> result = new List<string>();
-            result.Add($"   цена бумаги {ChangesOverPeriod(fromState.PriceWithoutCoupon / fromState.Quantity, currentState.CurrentAssetRate, true)}");
+            if (assetOnPeriodData.OnStart.Price > 0)
+                result.AddRange(assetOnPeriodData.FillChangesAndProfit());
+            return result;
+        }
+
+        private static List<string> CreateAtEndColumnForExistingAsset(this AssetOnPeriodData assetOnPeriodData)
+        {
+            List<string> result = new List<string>();
+
+            if (assetOnPeriodData.OnStart.Price > 0)
+                result.AddRange(assetOnPeriodData.FillChangesAndProfit());
+            return result;
+        }
+
+        private static List<string> FillState(this AssetState currentState)
+        {
+            List<string> result = new List<string>();
+            result.Add($"Количество: {currentState.Quantity} шт.");
+
+            if (currentState.Quantity > 0)
+            {
+                result.Add(currentState.Asset.AssetType == AssetType.Bond
+                    ? $" цена + НКД: ({currentState.CurrentAssetRate:N} + {currentState.AccumulatedCouponIncome:F}) = {currentState.AveragePriceStr}"
+                    : $" цена: {currentState.AveragePriceStr}");
+                result.Add($"Стоимость: {currentState.PriceStr}");
+            }
+            result.Add("");
+            return result;
+        }
+
+        private static List<string> FillChangesAndProfit(this AssetOnPeriodData assetOnPeriodData)
+        {
+            List<string> result = new List<string>();
+            var currentState = assetOnPeriodData.AtEnd;
+            result.Add($"За период {assetOnPeriodData.InBetween.Caption}");
+            result.AddRange(FillPeriodChanges(assetOnPeriodData.OnStart, currentState));
+            var profitOverPeriod = currentState.PriceInUsd - assetOnPeriodData.OnStart.PriceInUsd
+                                                           - assetOnPeriodData.InBetween.OperationFeesInUsd + assetOnPeriodData.InBetween.ReceivedCouponInUsd;
+            var resultStr = $"Результат в usd: {currentState.PriceInUsd:F} - {assetOnPeriodData.OnStart.PriceInUsd:F}";
+            if (assetOnPeriodData.InBetween.OperationFeesInUsd > 0)
+                resultStr += $" - {assetOnPeriodData.InBetween.OperationFeesInUsd:F}";
+            if (assetOnPeriodData.InBetween.ReceivedCouponInUsd > 0)
+                resultStr += $" + {assetOnPeriodData.InBetween.ReceivedCouponInUsd:F}";
+            result.Add(resultStr);
+            result.Add($"                        = {profitOverPeriod:N} usd  ({profitOverPeriod / assetOnPeriodData.OnStart.PriceInUsd * 100:F1}%)");
+            result.Add("");
+
+            result.Add("С момента приобретения");
+            result.AddRange(FillAllChanges(assetOnPeriodData));
+            var fullPriceBefore = assetOnPeriodData.Before.PriceInUsd + assetOnPeriodData.Before.OperationFeesInUsd;
+            var profitTotal = currentState.PriceInUsd + currentState.ReceivedCouponInUsd - fullPriceBefore;
+            var totalStr = $"Результат в usd: {currentState.PriceInUsd:F} - {assetOnPeriodData.Before.PriceInUsd:F} - {currentState.OperationFeesInUsd:F}";
+            if (currentState.ReceivedCouponInUsd > 0)
+                totalStr += $" + {currentState.ReceivedCouponInUsd:F}";
+            result.Add(totalStr);
+            result.Add($"                        = {profitTotal:N} usd ({profitTotal / assetOnPeriodData.Before.PriceInUsd * 100:F1}%)");
+            result.Add("");
+            return result;
+        }
+
+        private static List<string> FillAllChanges(AssetOnPeriodData assetOnPeriodData)
+        {
+            List<string> result = new List<string>();
+            var fromState = assetOnPeriodData.Before;
+            var currentState = assetOnPeriodData.AtEnd;
+            result.Add($"   цена бумаги {GetChangesLineForTwoFigures(fromState.PriceWithoutCoupon / fromState.Quantity, currentState.CurrentAssetRate, true)}");
             if (currentState.Asset.AssetType == AssetType.Bond)
             {
-                var lineWithCoupon = ChangesOverPeriod(
+                var lineWithCoupon = GetChangesLineForTwoFigures(
                     fromState.AveragePrice,
-                    currentState.AveragePrice + currentState.ReceivedCoupon / currentState.Quantity, true);
+                    currentState.AveragePrice + currentState.ReceivedCoupon / fromState.Quantity, true);
                 result.Add($"     (с учетом НКД {lineWithCoupon})");
             }
 
-            result.Add($"   цена пакета {ChangesOverPeriod(fromState.Price, currentState.Price, true)}");
-            if (currentState.Asset.TrustAccount.Currency != CurrencyCode.USD)
-                result.Add($"   цена пакета в usd {ChangesOverPeriod(fromState.PriceInUsd, currentState.PriceInUsd, true)}");
+            result.AddRange(GetChangesOfPackage(fromState, currentState));
             return result;
         }
 
         private static List<string> FillPeriodChanges(AssetState fromState, AssetState currentState)
         {
             List<string> result = new List<string>();
-            result.Add($"   цена бумаги {ChangesOverPeriod(fromState.CurrentAssetRate, currentState.CurrentAssetRate, true)}");
+            result.AddRange(GetChangesOfAsset(fromState, currentState));
+            result.AddRange(GetChangesOfPackage(fromState, currentState));
+            return result;
+        }
+
+        private static List<string> GetChangesOfAsset(AssetState fromState, AssetState currentState)
+        {
+            List<string> result = new List<string>();
+            result.Add($"   цена бумаги {GetChangesLineForTwoFigures(fromState.CurrentAssetRate, currentState.CurrentAssetRate, true)}");
             if (currentState.Asset.AssetType == AssetType.Bond)
             {
-                var lineWithCoupon = ChangesOverPeriod(
+                var lineWithCoupon = GetChangesLineForTwoFigures(
                     fromState.CurrentAssetRate + fromState.AccumulatedCouponIncome,
                     currentState.CurrentAssetRate + currentState.AccumulatedCouponIncome, true);
                 result.Add($"     (с учетом НКД {lineWithCoupon})");
             }
-
-            result.Add($"   цена пакета {ChangesOverPeriod(fromState.Price, currentState.Price, true)}");
-            if (currentState.Asset.TrustAccount.Currency != CurrencyCode.USD)
-                result.Add($"   цена пакета в usd {ChangesOverPeriod(fromState.PriceInUsd, currentState.PriceInUsd, true)}");
             return result;
         }
 
-        private static string ChangesOverPeriod(decimal a, decimal b, bool theMoreTheBetter)
+        private static List<string> GetChangesOfPackage(AssetState fromState, AssetState currentState)
+        {
+            List<string> result = new List<string>();
+            result.Add($"   цена пакета {GetChangesLineForTwoFigures(fromState.Price, currentState.Price, true)}");
+            if (currentState.Asset.TrustAccount.Currency != CurrencyCode.USD)
+                result.Add($"   цена пакета в usd {GetChangesLineForTwoFigures(fromState.PriceInUsd, currentState.PriceInUsd, true)}");
+            return result;
+        }
+
+        private static string GetChangesLineForTwoFigures(decimal a, decimal b, bool theMoreTheBetter)
         {
             if (a == 0) return "";
             var diff = theMoreTheBetter ? b - a : a - b;
             return $"{a:N} -> {b:N}  {diff / a * 100:F1}%";
-        }
-
-        private static IEnumerable<string> ToAssetAnalysis(this InvestTranModel tranModel)
-        {
-            string rate = tranModel.Currency != CurrencyCode.USD
-                ? $"(курс: {tranModel.Rate:F} rub купить 1 usd)"
-                : "";
-            string word;
-            if (tranModel.InvestOperationType == InvestOperationType.BuyStocks
-               || tranModel.InvestOperationType == InvestOperationType.BuyBonds)
-                word = "купил по";
-            else if (tranModel.InvestOperationType == InvestOperationType.SellStocks
-                || tranModel.InvestOperationType == InvestOperationType.SellBonds)
-                word = "продал по";
-            else if (tranModel.InvestOperationType == InvestOperationType.EnrollCouponOrDividends)
-                word = "получены дивы/купон";
-            else word = "not implemented yet";
-
-            decimal sum;
-            decimal oneAssetPrice;
-            string oneAssetPriceStr;
-            string oneAssetPriceStr2;
-            if (tranModel.InvestOperationType.IsBond())
-            {
-                oneAssetPrice = tranModel.CurrencyAmount / tranModel.AssetAmount +
-                                tranModel.CouponAmount / tranModel.AssetAmount;
-                sum = tranModel.CurrencyAmount + tranModel.CouponAmount;
-                oneAssetPriceStr = $"{tranModel.CurrencyAmount / tranModel.AssetAmount:N} + {tranModel.CouponAmount / tranModel.AssetAmount:N}";
-                oneAssetPriceStr2 = $" = {oneAssetPrice} {tranModel.Currency.ToString().ToLower()}";
-            }
-            else
-            {
-                oneAssetPrice = tranModel.CurrencyAmount / tranModel.AssetAmount;
-                sum = tranModel.CurrencyAmount;
-                oneAssetPriceStr = $"{tranModel.CurrencyAmount / tranModel.AssetAmount:N}";
-                oneAssetPriceStr2 = "";
-            }
-
-            var inCurrency = $"{oneAssetPrice:N} * {tranModel.AssetAmount} шт = {sum:N} {tranModel.Currency.ToString().ToLower()}";
-
-            if (tranModel.Asset.TrustAccount.Currency == CurrencyCode.USD)
-            {
-                yield return $"{tranModel.Timestamp:dd-MMM-yy} {word}";
-                yield return $"    {inCurrency}";
-            }
-            else
-            {
-                var inUsd = $" ({sum / tranModel.Rate:N} usd)";
-                var oneAssetPriceStrInUsd = $"({oneAssetPrice / tranModel.Rate:N} usd)";
-                yield return $"{tranModel.Timestamp:dd-MMM-yy} {rate}";
-                yield return $" {word} {oneAssetPriceStr}{oneAssetPriceStr2} {oneAssetPriceStrInUsd}";
-                yield return $"         {inCurrency} {inUsd}";
-            }
-            yield return "";
-        }
-
-
-        public static AssetOnPeriodReportModel CreateReport(this AssetOnPeriodData assetOnPeriodData)
-        {
-            return new AssetOnPeriodReportModel()
-            {
-                BeforeState = assetOnPeriodData.Before.CreateBeforeColumn().ToList(),
-                OnStartState = assetOnPeriodData.CreateOnStartColumn().ToList(),
-                InBetweenTrans = assetOnPeriodData.CreateInBetweenColumn().ToList(),
-                AtEndState = assetOnPeriodData.CreateAtEndColumn().ToList(),
-            };
         }
 
     }
---
+++ Keeper2018/Forms/Investments/AnalysisTools/TranToAssetAnalysis.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using KeeperDomain;
+
+namespace Keeper2018
+{
+    public static class TranToAssetAnalysis
+    {
+        public static IEnumerable<string> ToAssetAnalysis(this InvestTranModel tranModel)
+        {
+            string word = tranModel.WordsForOperationType();
+
+            decimal sum;
+            decimal oneAssetPrice;
+            string oneAssetPriceStr;
+            string oneAssetPriceStr2;
+            if (tranModel.InvestOperationType.IsBond())
+            {
+                oneAssetPrice = tranModel.CurrencyAmount / tranModel.AssetAmount +
+                                tranModel.CouponAmount / tranModel.AssetAmount;
+                sum = tranModel.CurrencyAmount + tranModel.CouponAmount;
+                oneAssetPriceStr = $"{tranModel.CurrencyAmount / tranModel.AssetAmount:N} + {tranModel.CouponAmount / tranModel.AssetAmount:N}";
+                oneAssetPriceStr2 = $" = {oneAssetPrice} {tranModel.Currency.ToString().ToLower()}";
+            }
+            else
+            {
+                oneAssetPrice = tranModel.CurrencyAmount / tranModel.AssetAmount;
+                sum = tranModel.CurrencyAmount;
+                oneAssetPriceStr = $"{tranModel.CurrencyAmount / tranModel.AssetAmount:N}";
+                oneAssetPriceStr2 = "";
+            }
+
+            var inCurrency = $"{oneAssetPrice:N} * {tranModel.AssetAmount} шт = {sum:N} {tranModel.Currency.ToString().ToLower()}";
+
+            if (tranModel.Asset.TrustAccount.Currency == CurrencyCode.USD)
+            {
+                yield return $"{tranModel.Timestamp:dd-MMM-yy} {word}";
+                yield return $"    {inCurrency}";
+            }
+            else
+            {
+                string rate = $"(курс: {tranModel.Rate:F} rub купить 1 usd)";
+                var inUsd = $" ({sum / tranModel.Rate:N} usd)";
+                var oneAssetPriceStrInUsd = $"({oneAssetPrice / tranModel.Rate:N} usd)";
+                yield return $"{tranModel.Timestamp:dd-MMM-yy} {rate}";
+                yield return $" {word} {oneAssetPriceStr}{oneAssetPriceStr2} {oneAssetPriceStrInUsd}";
+                yield return $"         {inCurrency} {inUsd}";
+            }
+            yield return "";
+        }
+
+        private static string WordsForOperationType(this InvestTranModel tranModel)
+        {
+            switch (tranModel.InvestOperationType)
+            {
+                case InvestOperationType.BuyStocks:
+                case InvestOperationType.BuyBonds:
+                    return "купил по";
+                case InvestOperationType.SellStocks:
+                case InvestOperationType.SellBonds:
+                    return "продал по";
+                case InvestOperationType.EnrollCouponOrDividends:
+                    return "получены дивы/купон";
+                default:
+                    return "not implemented yet";
+            }
+        }
+    }
+}
