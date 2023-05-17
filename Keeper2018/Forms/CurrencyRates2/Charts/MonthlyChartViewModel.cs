df08b7c6feebec53d35b8eb3a700d71125d2db54
mli2805 <mli2805@gmail.com>
2023-05-17 09:52:31 +0300

    monthly usd chart improved

--- Keeper2018/Forms/CurrencyRates2/Charts/MonthlyChartViewModel.cs
+++ Keeper2018/Forms/CurrencyRates2/Charts/MonthlyChartViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Caliburn.Micro;
+using KeeperDomain;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
@@ -26,48 +28,60 @@ namespace Keeper2018
             _caption = caption;
             _keeperDataModel = keeperDataModel;
 
-            var date = new DateTime(2020, 4, 1);
-            do
-            {
-                MyPlotModel00.Series.Add(OneMonthOfUsd(date));
-                date = date.AddMonths(1);
-            } while (date < new DateTime(2020, 7, 1));
-            // MyPlotModel00.Axes.Add(new LinearAxis(){ Minimum = 2.515, Maximum = 2.67 });
+            var startDate = DateTime.Now.AddMonths(-12).GetStartOfMonth();
 
-           do
-            {
-                MyPlotModel01.Series.Add(OneMonthOfUsd(date));
-                date = date.AddMonths(1);
-            } while (date < new DateTime(2020, 10, 1));
-            // MyPlotModel01.Axes.Add(new LinearAxis(){ Minimum = 2.515, Maximum = 2.67 });
+            var l00 = GetSeriesByMonths(startDate, startDate.AddMonths(3), out double min, out double max);
+            l00.ForEach(MyPlotModel00.Series.Add);
+            MyPlotModel00.Axes.Add(new LinearAxis() { Minimum = min, Maximum = max });
 
-           do
-            {
-                MyPlotModel10.Series.Add(OneMonthOfUsd(date));
-                date = date.AddMonths(1);
-            } while (date < new DateTime(2021, 1, 1));
-            MyPlotModel10.Axes.Add(new LinearAxis(){ Minimum = 2.515, Maximum = 2.67 });
+            var l01 = GetSeriesByMonths(startDate.AddMonths(3), startDate.AddMonths(6), out min, out max);
+            l01.ForEach(MyPlotModel01.Series.Add);
+            MyPlotModel01.Axes.Add(new LinearAxis() { Minimum = min, Maximum = max });
 
-            do
-            {
-                MyPlotModel11.Series.Add(OneMonthOfUsd(date));
-                date = date.AddMonths(1);
-            } while (date <= DateTime.Now);
-            MyPlotModel11.Axes.Add(new LinearAxis(){ Minimum = 2.515, Maximum = 2.67 });
+            var l10 = GetSeriesByMonths(startDate.AddMonths(6), startDate.AddMonths(9), out min, out max);
+            l10.ForEach(MyPlotModel10.Series.Add);
+            MyPlotModel10.Axes.Add(new LinearAxis() { Minimum = min, Maximum = max });
+
+            var l11 = GetSeriesByMonths(startDate.AddMonths(9), DateTime.Today, out min, out max);
+            l11.ForEach(MyPlotModel11.Series.Add);
+            MyPlotModel11.Axes.Add(new LinearAxis() { Minimum = min, Maximum = max });
         }
 
 
-        private LineSeries OneMonthOfUsd(DateTime start)
+        private List<LineSeries> GetSeriesByMonths(DateTime start, DateTime end, out double min, out double max)
+        {
+            var result = new List<LineSeries>();
+            min = double.MaxValue;
+            max = 0;
+            while (start < end)
+            {
+                var ls = OneMonthOfUsd(start, out double lmin, out double lmax);
+                result.Add(ls);
+                if (lmin < min) min = lmin;
+                if (lmax > max) max = lmax;
+
+                start = start.AddMonths(1);
+            }
+
+            return result;
+        }
+
+        private LineSeries OneMonthOfUsd(DateTime start, out double min, out double max)
         {
             var year = start.Year;
             var month = start.Month;
             var result = new LineSeries() { Title = $"{start:MMM yyyy}" };
-            foreach (var line in _keeperDataModel.ExchangeRates.Values.Where(r => r.Date.Year == year && r.Date.Month == month))
+            var lines = _keeperDataModel.ExchangeRates
+                .Values.Where(r => r.Date.Year == year && r.Date.Month == month).ToList();
+            foreach (var line in lines)
             {
                 var rate = line.UsdToByn;
                 if (rate > 0)
                     result.Points.Add(new DataPoint(line.Date.Day, rate));
             }
+
+            max = lines.Max(r => r.UsdToByn);
+            min = lines.Min(r => r.UsdToByn);
             return result;
         }
 
