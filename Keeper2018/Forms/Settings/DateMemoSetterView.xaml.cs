8ac21680f6273423904d5fd67a869597c6aa0a77
mli2805 <mli2805@gmail.com>
2023-09-25 19:55:50 +0300

    settings form with controls on tab items

---
+++ Keeper2018/Forms/Settings/DateMemoSetterView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Keeper2018
+{
+    /// <summary>
+    /// Interaction logic for DateMemoSetterView.xaml
+    /// </summary>
+    public partial class DateMemoSetterView
+    {
+        public DateMemoSetterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Keeper2018/Forms/Settings/SettingsViewModel.cs
+++ Keeper2018/Forms/Settings/SettingsViewModel.cs
@@ -4,5 +4,9 @@ namespace Keeper2018
 {
     public class SettingsViewModel : Screen
     {
+        protected override void OnViewLoaded(object view)
+        {
+            DisplayName = "Настрйки";
+        }
     }
 }
