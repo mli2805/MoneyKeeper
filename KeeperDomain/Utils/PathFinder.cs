325312c5e576c65b3f5cd4d57012f7c394e87731
mli2805 <mli2805@gmail.com>
2024-03-29 09:21:03 +0300

    temporary DB path

--- KeeperDomain/Utils/PathFinder.cs
+++ KeeperDomain/Utils/PathFinder.cs
@@ -10,8 +10,9 @@ namespace KeeperDomain
     {
         public static string GetKeeper2018BasePath()
         {
-//           return Path.Combine(GetDropboxPath(), @"Keeper2018");
-            return Path.Combine(@"d:\Google Drive", @"Keeper2018");
+            //return Path.Combine(@"d:\Google Drive", @"Keeper2018");
+            // на время разработки новой фичи
+            return @"c:\VsGitProjects\Keeper\Keeper2018\bin\";
         }
 
         /// <summary>
