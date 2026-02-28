f1ccb5c211ce8d7cbe3333f8ef637c1a10c69865
leanid-marholin <lmarholin@veexinc.com>
2026-02-28 16:20:26 +0300

    minor

--- Keeper2018/Forms/Shell/ShellViewModel.cs
+++ Keeper2018/Forms/Shell/ShellViewModel.cs
@@ -38,9 +38,7 @@ namespace Keeper2018
 
         protected override async void OnViewLoaded(object view)
         {
-
-
-            DisplayName = "Keeper 2018";
+            DisplayName = "Keeper 2018 (old)";
             _dbLoaded = await _dbLoader.LoadAndExpand();
             if (!_dbLoaded)
             {
